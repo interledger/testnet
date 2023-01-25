f2da55a35e50dec8b34d928aaf524c3fa5f88653
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-01-25 13:11:59 +0200

    feat(wallet): add button component

---
+++ packages/wallet/src/ui/Button.tsx
@@ -0,0 +1,62 @@
+import { cva, cx, type VariantProps } from 'class-variance-authority'
+import { forwardRef } from 'react'
+
+import { ButtonOrLink, type ButtonOrLinkProps } from './ButtonOrLink'
+
+/**
+ * CVA helps us define variants for our components.
+ *
+ * For this button component, we created two variants: `intent` and `size`.
+ *
+ * The `VariantProps` helper is going to extract the variants types so we can
+ * safely type our button.
+ *
+ * Usage:
+ *  - <Button intent='primary' size='md' className="text-bold">My Button</Button>
+ *
+ * We can define `defaultVariants` for the component. In our case, if there is
+ * no `intent` or `size` passed in, they will use the default variants specified
+ * in `defaultVariants` object (in our case `intent -> primary` and `size => md`).
+ *
+ * Example:
+ *  - <Button>My Button</Button>
+ */
+
+const buttonStyles = cva(['inline-flex items-center justify-center'], {
+  variants: {
+    intent: {
+      primary: ['text-white bg-gradient-to-r from-[#00B1D8] to-[#2EC08C]'],
+      outline: [
+        'bg-transparent text-orange-500 border border-orange-500 hover:text-white hover:bg-orange-500'
+      ]
+    },
+    size: {
+      md: 'px-3 py-2 rounded-md font-medium'
+    }
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'md'
+  }
+})
+
+type ButtonProps = VariantProps<typeof buttonStyles> &
+  ButtonOrLinkProps & {
+    ['aria-label']: string
+  }
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ intent, size, children, className, ...props }, ref) => {
+    return (
+      <ButtonOrLink
+        ref={ref}
+        className={cx(className, buttonStyles({ intent, size }))}
+        {...props}
+      >
+        {children}
+      </ButtonOrLink>
+    )
+  }
+)
+
+Button.displayName = 'Button'
