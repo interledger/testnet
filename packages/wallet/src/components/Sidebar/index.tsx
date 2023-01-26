e5ce8baea727af8e4e81c96c50060417d0349dd2
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-01-26 12:54:49 +0200

    feat(wallet): sidebar refactor

--- packages/wallet/src/components/Sidebar/index.tsx
+++ packages/wallet/src/components/Sidebar/index.tsx
@@ -16,7 +16,7 @@ const navigationItems: NavigationItemProps[] = [
     href: '/accounts'
   },
   {
-    name: 'Developer',
+    name: 'Developer Account',
     href: '/developer'
   },
   {
@@ -32,24 +32,20 @@ const navigationItems: NavigationItemProps[] = [
 const Sidebar = () => {
   return (
     <div className="hidden md:fixed md:inset-y-0 md:flex md:w-60 md:flex-col">
-      <div className="flex min-h-0 flex-1 flex-col bg-gradient-to-r from-[#00B1D8] to-[#6AC1B7]">
-        <div className="flex flex-1 flex-col overflow-y-auto pt-10 pb-4">
-          <div className="flex flex-shrink-0 items-center px-10">
-            <Logo className="h-16 w-16" />
-          </div>
-          <nav className="mt-14 flex-1 space-y-8 px-10">
-            {navigationItems.map((navItem) => (
-              <Link
-                key={navItem.name}
-                href={navItem.href}
-                className="flex text-lg font-semibold text-gray-100 text-opacity-80 hover:text-white"
-              >
-                {/* Icon here */}
-                {navItem.name}
-              </Link>
-            ))}
-          </nav>
-        </div>
+      <div className="flex min-h-0 flex-1 flex-col overflow-y-auto bg-gradient-to-r from-[#00B1D8] to-[#6AC1B7] p-10">
+        <Logo className="h-16 w-16" />
+        <nav className="mt-14 flex-1  space-y-8">
+          {navigationItems.map((navItem) => (
+            <Link
+              key={navItem.name}
+              href={navItem.href}
+              className="flex text-lg font-semibold text-gray-100 text-opacity-80 hover:text-white"
+            >
+              {/* Icon here */}
+              {navItem.name}
+            </Link>
+          ))}
+        </nav>
       </div>
     </div>
   )
