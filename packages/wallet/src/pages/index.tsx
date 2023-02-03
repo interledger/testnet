d68c3d07c853bf9057af402e2006e8cfdba4b57f
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-02-03 12:37:27 +0200

    feat(wallet): add responsive menu (#39)

--- packages/wallet/src/pages/index.tsx
+++ packages/wallet/src/pages/index.tsx
@@ -1,7 +1,5 @@
+import { AppLayout } from '@/components/layouts/AppLayout'
+
 export default function Home() {
-  return (
-    <main className="min-w-screen grid min-h-screen place-items-center bg-red-200 text-4xl">
-      Interledger Wallet
-    </main>
-  )
+  return <AppLayout>Content</AppLayout>
 }
