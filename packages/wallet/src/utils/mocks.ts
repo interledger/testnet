10bb70d2e98152b21a9e1a4fb6d547d33655ec43
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-02-03 16:07:18 +0200

    add homepage

--- packages/wallet/src/utils/mocks.ts
+++ packages/wallet/src/utils/mocks.ts
@@ -7,17 +7,21 @@ export type Account = {
   id: string
   name: string
   asset: Asset
+  balance: number
 }
+
+const assetCodes = ['EUR', 'USD', 'XRP', 'CAD', 'GBP']
 
 export const mockAccount = (): Account => ({
   id: Math.random().toString(36).slice(2, -1),
   name: `Account ${Math.floor(Math.random() * 100) + 1}`,
-  asset: mockAsset()
+  asset: mockAsset(),
+  balance: parseFloat((Math.random() * 1000 + 100).toPrecision(5))
 })
 
 export const mockAsset = (): Asset => ({
   scale: Math.floor(Math.random() * 10) + 1,
-  code: (Math.random() + 1).toString(36).slice(2, 5).toUpperCase()
+  code: assetCodes[Math.floor(Math.random() * assetCodes.length)]
 })
 
 export const mockAccountList = (): Account[] => [
