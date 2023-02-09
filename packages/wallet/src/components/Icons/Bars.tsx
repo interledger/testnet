4d2c242234ea5af4ebfb1a76849a15efb1fd0ec4
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-02-09 18:09:35 +0200

    add icons

--- packages/wallet/src/components/Icons/Bars.tsx
+++ packages/wallet/src/components/Icons/Bars.tsx
@@ -6,14 +6,18 @@ export const Bars = (props: SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
+      stroke="url(#bars-blue-green)"
+      strokeWidth={1.5}
       {...props}
     >
+      <linearGradient id="bars-blue-green" x1="100%" y1="100%" x2="0%" y2="0%">
+        <stop stopColor="#6dd5ed" offset="0%" />
+        <stop stopColor="#2193b0" offset="100%" />
+      </linearGradient>
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+        d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
       />
     </svg>
   )
