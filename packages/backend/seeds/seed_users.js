4e7677410901ccd4b67658e82ffda64e4ba4bdbc
beniaminmunteanu <beniamin.munteanu@xpsol.ro>
2023-02-14 22:34:44 +0200

    remove this branche's database config and migrations, since main had them merged already from another PR (in a different folder structure)

--- packages/backend/seeds/seed_users.js
+++ packages/backend/seeds/seed_users.js
@@ -11,60 +11,60 @@ exports.seed = async function (knex) {
       username: 'admin',
       email: 'admin@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'admin',
-      last_name: 'admin',
-      rapyd_wallet_id: '1',
-      rapyd_contact_id: '1'
+      firstName: 'admin',
+      lastName: 'admin',
+      rapydWalletId: '1',
+      rapydContactId: '1'
     },
     {
       id: 2,
       username: 'user2',
       email: 'user2@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'user2',
-      last_name: 'user2',
-      rapyd_wallet_id: '2',
-      rapyd_contact_id: '2'
+      firstName: 'user2',
+      lastName: 'user2',
+      rapydWalletId: '2',
+      rapydContactId: '2'
     },
     {
       id: 3,
       username: 'user3',
       email: 'user3@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'user3',
-      last_name: 'user3',
-      rapyd_wallet_id: '3',
-      rapyd_contact_id: '3'
+      firstName: 'user3',
+      lastName: 'user3',
+      rapydWalletId: '3',
+      rapydContactId: '3'
     },
     {
       id: 4,
       username: 'user4',
       email: 'user4@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'user4',
-      last_name: 'user4',
-      rapyd_wallet_id: '4',
-      rapyd_contact_id: '4'
+      firstName: 'user4',
+      lastName: 'user4',
+      rapydWalletId: '4',
+      rapydContactId: '4'
     },
     {
       id: 5,
       username: 'user5',
       email: 'user5@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'user5',
-      last_name: 'user5',
-      rapyd_wallet_id: '5',
-      rapyd_contact_id: '5'
+      firstName: 'user5',
+      lastName: 'user5',
+      rapydWalletId: '5',
+      rapydContactId: '5'
     },
     {
       id: 6,
       username: 'user6',
       email: 'user6@admin.com',
       password: '$2a$10$p6Rzwj/YcoPBdh3MN4wKDOIaiPwBcZOvFlwDqH.6jZ3Qm.yX0Z0dq',
-      first_name: 'user6',
-      last_name: 'user6',
-      rapyd_wallet_id: '6',
-      rapyd_contact_id: '6'
+      firstName: 'user6',
+      lastName: 'user6',
+      rapydWalletId: '6',
+      rapydContactId: '6'
     }
   ])
 }
