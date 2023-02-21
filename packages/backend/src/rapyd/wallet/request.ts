94d64f6ab51725bc66f52dfdd719cf267870dc4a
Rico <rico.gonzalez1013@gmail.com>
2023-02-21 12:04:01 +0200

    feat(backend): moved rapyd folder to src

--- packages/backend/src/rapyd/wallet/request.ts
+++ packages/backend/src/rapyd/wallet/request.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const END_POINT = 'https://sandboxapi.rapyd.net/v1'
+const RAPYD_ENDPOINT = process.env.RAPYD_API
 
 // const makeRapydPostRequest = async (_endpoint: string, _body: string) => {
 // }
 
 const createRapydWallet = async (wallet: RapydWallet) => {
-  await axios.post(`${END_POINT}/user`, wallet)
+  await axios.post(`${RAPYD_ENDPOINT}/user`, wallet)
 }
 
 export { createRapydWallet }
