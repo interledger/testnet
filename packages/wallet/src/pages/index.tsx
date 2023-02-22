5e6db5df3ecdbaa8b17aef05f06361d1d5169e1a
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-02-22 16:41:46 +0200

    design updates

--- packages/wallet/src/pages/index.tsx
+++ packages/wallet/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { AccountCard } from '@/components/AccountCard'
-import { Cog } from '@/components/icons/Cog'
 import { New } from '@/components/icons/New'
 import { Pay } from '@/components/icons/Pay'
 import { Receive } from '@/components/icons/Receive'
@@ -28,51 +27,48 @@ export default function Home({ accounts }: HomeProps) {
           <p className="text-2xl font-semibold md:text-4xl">$2,934</p>
         </div>
       </div>
-      <div className="my-5 flex flex-col space-y-5">
-        <div className="grid grid-cols-4 gap-2">
+      <div className="flex w-full flex-col space-y-3 md:max-w-md">
+        <div className="mt-2 grid grid-cols-4 gap-2">
           <Link className="group flex aspect-square flex-col items-center justify-center rounded-lg border border-green-5 bg-white shadow-md hover:border-green-6">
-            <Pay />
+            <Pay className="h-8 w-8" />
             <span className="font-medium text-green-5 group-hover:text-green-6">
               Send
             </span>
           </Link>
           <Link className="group flex aspect-square flex-col items-center justify-center rounded-lg border border-green-5 bg-white shadow-md hover:border-green-6">
-            <Send />
+            <Send className="h-8 w-8" />
             <span className="font-medium text-green-5 group-hover:text-green-6">
               Pay
             </span>
           </Link>
           <Link className="group flex aspect-square flex-col items-center justify-center rounded-lg border border-green-5 bg-white shadow-md hover:border-green-6">
-            <Receive />
+            <Receive className="h-8 w-8" />
             <span className="font-medium text-green-5 group-hover:text-green-6">
               Receive
             </span>
           </Link>
           <Link className="group flex aspect-square flex-col items-center justify-center rounded-lg border border-green-5 bg-white shadow-md hover:border-green-6">
-            <New />
+            <New className="h-8 w-8" />
             <span className="font-medium text-green-5 group-hover:text-green-6">
               New account
             </span>
           </Link>
         </div>
-      </div>
-      <div className="flex w-full flex-col space-y-2 md:max-w-lg">
         <div className="flex items-center justify-between">
           <h3 className="text-lg font-semibold leading-none text-green">
             My Accounts
           </h3>
-          <Cog className="h-8 w-8 text-green" />
         </div>
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-2">
           {accounts.map((account) => (
             <Link
               href={`accounts/${account.id}`}
               key={account.id}
-              className={`rounded-lg
+              className={`
+              rounded-lg shadow-sm transition-transform hover:scale-105 hover:shadow-md
               [&:nth-child(4n+1)]:bg-gradient-primary 
               [&:nth-child(4n+2)]:bg-gradient-violet 
-              [&:nth-child(4n+3)]:bg-gradient-pink 
-              [&:nth-child(4n+4)]:bg-gradient-orange`}
+              [&:nth-child(4n+3)]:bg-gradient-pink [&:nth-child(4n+4)]:bg-gradient-orange `}
             >
               <AccountCard
                 name={account.name}
