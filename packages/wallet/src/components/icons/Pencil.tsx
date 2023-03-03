280997d38218804c0d0f497b1ed00e566ab252da
Timea <117268143+Tymmmy@users.noreply.github.com>
2023-03-03 16:27:28 +0200

    fix(wallet): fix issue with Safari and svg element (#147)

--- packages/wallet/src/components/icons/Pencil.tsx
+++ packages/wallet/src/components/icons/Pencil.tsx
@@ -6,6 +6,8 @@ export const PencilSquare = (props: SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
+      width={24}
+      height={24}
       strokeWidth={1.5}
       stroke="currentColor"
       {...props}
--- packages/wallet/src/components/icons/X.tsx
+++ packages/wallet/src/components/icons/X.tsx
@@ -6,6 +6,8 @@ export const X = (props: SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
+      width={24}
+      height={24}
       stroke="url(#x-blue-green)"
       strokeWidth={1.5}
       {...props}
