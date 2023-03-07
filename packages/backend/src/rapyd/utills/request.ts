20d5c6200fbb1f0a4a2c09a0c9aa4f5287dffc45
DOMIN <101926344+domin191013@users.noreply.github.com>
2023-03-07 21:00:53 +0900

    feat(backend): added createWallet api (#143)

--- packages/backend/src/rapyd/utills/request.ts
+++ packages/backend/src/rapyd/utills/request.ts
@@ -1,101 +1,71 @@
 import axios from 'axios'
-import crypto from 'crypto'
 import env from '../../config/env'
+import crypto from 'crypto-js'
 
-const generateRandomString = (size: number) => {
-  try {
-    return crypto.randomBytes(size).toString('hex')
-  } catch (error) {
-    console.error('Error generating salt')
-    throw error
-  }
-}
-
-const getRandomArbitrary = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min)) + min
-}
-
-const getSaltKey = () => {
-  const length = getRandomArbitrary(8, 16)
-  return generateRandomString(length)
-}
-
-const getCurrentTimestamp = () => {
-  return Math.round(new Date().getTime() / 1000) // Current Unix time (seconds).
-}
-
-const getIdempotency = () => {
-  return new Date().getTime().toString()
-}
-
-const sign = (
-  method: string,
+const calcSignature = (
+  httpMethod: string,
   url: string,
   salt: string,
-  timestamp: number,
+  accessKey: string,
+  secretKey: string,
   body: string
-) => {
-  const urlPath = `/v1/${url}`
-
-  try {
-    const bodyString = body == '{}' ? '' : body
-
-    const toSign =
-      method.toLowerCase() +
-      urlPath +
-      salt +
-      timestamp +
-      env.RAPYD_ACCESS_KEY +
-      env.RAPYD_SECRET_KEY +
-      bodyString
-    console.log(`toSign: ${toSign}`)
-
-    const hash = crypto.createHmac(
-      'sha256',
-      Buffer.from(env.RAPYD_SECRET_KEY ?? '')
-    )
-    hash.update(toSign)
-    const signature = Buffer.from(hash.digest('hex')).toString('base64')
-    console.log(`signature: ${signature}`)
-
-    return signature
-  } catch (error) {
-    console.error('Error generating signature')
-    throw error
-  }
+): string => {
+  const timestamp: string = (
+    Math.floor(new Date().getTime() / 1000) - 10
+  ).toString()
+  const toSign: string =
+    httpMethod + url + salt + timestamp + accessKey + secretKey + body
+  const signature = crypto.enc.Hex.stringify(
+    crypto.HmacSHA256(toSign, secretKey)
+  )
+  return crypto.enc.Base64.stringify(crypto.enc.Utf8.parse(signature))
 }
 
-const getRapydRequestHeader = (url: string, body: string) => {
-  const salt = getSaltKey()
-  const timestamp = getCurrentTimestamp()
-  const signature = sign('get', url, salt, timestamp, body)
+const getRapydRequestHeader = (method: string, url: string, body: string) => {
+  const salt = crypto.lib.WordArray.random(12).toString()
+  const timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString()
+
+  const signature = calcSignature(
+    method,
+    `/v1/${url}`,
+    salt,
+    env.RAPYD_ACCESS_KEY,
+    env.RAPYD_SECRET_KEY,
+    body
+  )
 
   return {
     'Content-Type': 'application/json',
     'access_key': env.RAPYD_ACCESS_KEY,
     salt,
-    'timestamp': getCurrentTimestamp(),
-    signature,
-    'idempotency': getIdempotency()
+    timestamp,
+    signature
   }
 }
 
-const makeRapydGetRequest = (url: string) => {
-  const headers = getRapydRequestHeader(url, '')
+const makeRapydGetRequest = async (url: string) => {
+  const headers = getRapydRequestHeader('get', url, '')
 
-  console.log(headers)
-
-  return axios.get(`${env.RAPYD_API}/${url}`, {
+  const res = await axios.get(`${env.RAPYD_API}/${url}`, {
     headers
   })
+
+  return res.data
 }
 
-const makeRapydPostRequest = (url: string, body: string) => {
-  const headers = getRapydRequestHeader(url, body)
+const makeRapydPostRequest = async (url: string, body: string) => {
+  const headers = getRapydRequestHeader('post', url, body)
 
-  return axios.post(`${env.RAPYD_API}/${url}`, JSON.parse(body), {
-    headers
-  })
+  try {
+    const res = await axios.post(`${env.RAPYD_API}/${url}`, JSON.parse(body), {
+      headers
+    })
+
+    return res.data
+  } catch (e) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (e as any).response.data
+  }
 }
 
 export { makeRapydGetRequest, makeRapydPostRequest }
---
+++ packages/backend/src/wallet/schemas/walletSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const walletSchema = z.object({
+  firstName: z.string({ required_error: 'First name is required' }),
+  lastName: z.string({ required_error: 'Last name is required' }),
+  email: z.string().email(),
+  address: z.string({ required_error: 'Address is required' }),
+  city: z.string({ required_error: 'City is required' }),
+  country: z.string({ required_error: 'Country Code is required' }),
+  zip: z.string({ required_error: 'Zipcode is required' }),
+
+  phone: z.string().optional()
+})
