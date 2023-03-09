7821e8d9e14f87f51d1b0fbfabde3479497974fd
beniaminmunteanu <beniamin.munteanu@xpsol.ro>
2023-03-09 18:15:32 +0200

    implement first integration tests

--- packages/backend/tests/auth/auth.service.test.ts
+++ packages/backend/tests/auth/auth.service.test.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
-import { login } from '../../src/auth/auth.service'
-// import { User } from '../../src/user/models/user'
+import { signup } from '../../src/auth/auth.service'
 
 import {
   ContainerInstance,
@@ -9,48 +8,7 @@ import {
   TEST_TIMEOUTS
 } from '../test-setup'
 
-// import { Model, QueryBuilder } from 'objection'
-
-// const u = {
-//   id: 1,
-//   email: '1',
-//   password: '1',
-//   refreshTokens: [{ id: 1, token: '1', userId: 1, expiresAt: new Date() }]
-// } as unknown as User
-
-// console.log(u)
-// console.log(Model)
-
-// jest.mock('objection', () => {
-//   return jest.fn().mockImplementation(() => {
-//     return {
-//       findOne: jest.fn().mockImplementation(() => Promise.resolve([u])),
-//       select: jest.fn().mockImplementation(() => Promise.resolve(u)),
-//       withGraphFetched: jest.fn().mockImplementation(() => Promise.resolve(u))
-//     }
-//   })
-// })
-
-// jest.mock('../../src/user/models/user')
-// jest.mock('../../src/auth/models/refreshToken')
-
-// jest.mock('objection')
-// jest.mock('../../src/auth/models/refreshToken', () => {
-//   return jest.fn().mockImplementation(() => {
-//     return { id: 1, token: '1', userId: 1, expiresAt: new Date() }
-//   })
-// })
-
-// const uz = jest.mock('../../src/user/models/user', () => {
-//   return jest.fn().mockImplementation(() => {
-//     return {
-//       verifyPassword: jest.fn().mockImplementation(() => Promise.resolve(true)),
-//       query: jest.fn().mockImplementation(() => new QueryBuilder<any>())
-//     }
-//   })
-// })
-
-// console.log(uz)
+import { BadRequestException } from '../../src/shared/models/errors/BadRequestException'
 
 describe('AuthService', () => {
   jest.setTimeout(TEST_TIMEOUTS)
@@ -62,112 +20,54 @@ describe('AuthService', () => {
 
   afterAll(async () => await stopContainer(containerInstance))
 
-  it('logins', async () => {
-    const x = true
-
-    expect(x).toBe(true)
-    const mockReq = {
-      body: {
-        email: 'test@test.com',
-        password: 'password'
+  describe('signup', () => {
+    it('should return a token and refreshToken when valid credentials are supplied.', async () => {
+      const mockReq = {
+        body: {
+          email: 'test@test.com',
+          password: 'Admin1234',
+          confirmPassword: 'Admin1234'
+        }
+      } as Request
+      const mockRes = {
+        json: jest.fn(),
+        status: jest.fn()
       }
-    } as unknown as Request
+      mockRes.status.mockImplementation(() => mockRes)
+      await signup(
+        mockReq,
+        mockRes as unknown as Response,
+        jest.fn() as NextFunction
+      )
 
-    const mockRes = {
-      json: jest.fn()
-    } as unknown as Response
+      expect(mockRes.json).toHaveBeenCalledWith({
+        success: true,
+        message: 'Success'
+      })
+    })
 
-    const nextFn = jest.fn() as NextFunction
-
-    await login(mockReq, mockRes, nextFn)
+    it('should throw a BadRequestException if passwordConfirmation is invalid ', async () => {
+      const mockReq = {
+        body: {
+          email: 'test@test.com',
+          password: 'Admin1234',
+          confirmPassword: 'wrongPass'
+        }
+      } as Request
+      const mockRes = {
+        json: jest.fn(),
+        status: jest.fn()
+      }
+      const nextFn = jest.fn()
+      mockRes.status.mockImplementation(() => mockRes)
+      await signup(
+        mockReq,
+        mockRes as unknown as Response,
+        nextFn as NextFunction
+      )
+      expect(nextFn).toHaveBeenCalledWith(
+        new BadRequestException('Passwords do not match')
+      )
+    })
   })
-
-  // describe('login', () => {
-  //   it('should return a token and refreshToken when valid credentials are supplied', async () => {
-  //     const mockReq = {
-  //       body: {
-  //         email: 'test@test.com',
-  //         password: 'password'
-  //       }
-  //     } as Request
-  //     const mockRes = {
-  //       json: jest.fn()
-  //     } as unknown as Response
-
-  //     jest
-  //       .spyOn(User, 'query')
-  //       .mockImplementation(() => new QueryBuilder().resolve(u))
-
-  //     const nextFn = jest.fn() as NextFunction
-  //     await login(mockReq, mockRes, nextFn)
-
-  //     expect(mockRes.json).toHaveBeenCalledWith(
-  //       expect.objectContaining({
-  //         token: expect.any(String),
-  //         refreshToken: expect.any(String)
-  //       })
-  //     )
-  //   })
-
-  //   it('should throw an UnauthorisedException if the credentials are incorrect', async () => {
-  //     const mockReq = {
-  //       body: {
-  //         email: 'test@test.com',
-  //         password: 'wrongpassword'
-  //       }
-  //     } as Request
-  //     const mockRes = {
-  //       json: jest.fn(),
-  //       status: jest.fn(() => ({ json: jest.fn() }))
-  //     } as unknown as Response
-  //     await expect(login(mockReq, mockRes)).rejects.toThrow(
-  //       UnauthorisedException
-  //     )
-  //   })
-  // })
-
-  // describe('signup', () => {
-  //   it('should return a token and refreshToken when valid credentials are supplied', async () => {
-  //     const mockReq = {
-  //       body: {
-  //         email: 'test@test.com',
-  //         password: 'password'
-  //       }
-  //     } as Request
-  //     const mockRes = {
-  //       json: jest.fn()
-  //     } as unknown as Response
-  //     await signup(mockReq, mockRes)
-
-  //     expect(mockRes.json).toHaveBeenCalledWith(
-  //       expect.objectContaining({
-  //         token: expect.any(String),
-  //         refreshToken: expect.any(String)
-  //       })
-  //     )
-  //   })
-
-  //   it('should throw a BadRequestException if the email already exists', async () => {
-  //     const mockReq = {
-  //       body: {
-  //         email: 'test@test.com',
-  //         password: 'password'
-  //       }
-  //     } as Request
-  //     const mockRes = {
-  //       json: jest.fn(),
-  //       status: jest.fn(() => ({ json: jest.fn() }))
-  //     } as unknown as Response
-  //     await signup(mockReq, mockRes)
-
-  //     const mockReq2 = {
-  //       body: {
-  //         email: 'test@test.com',
-  //         password: 'password'
-  //       }
-  //     } as Request
-  //     await expect(signup(mockReq2, mockRes)).rejects.toThrow(
-  //       BadRequestException
-  //     )
-  //   })
 })
--- packages/backend/tests/test-setup.ts
+++ packages/backend/tests/test-setup.ts
@@ -10,40 +10,42 @@ export interface ContainerInstance {
   pg: Knex
 }
 
-/**
- * This function starts a test container with a postgres database with latest migrations and initializes the postgres knex client.
- *
- * @returns ContainerInstance containing pg client and test container
- */
 export async function startContainer(): Promise<ContainerInstance> {
-  const container = await new PostgreSqlContainer('postgres:11.11-alpine')
-    .withDatabase('testnet')
-    .withPassword('password')
-    .withUser('postgres')
-    // .withTmpFs({ '/temp_pgdata': 'rw,noexec,nosuid' })
-    .start()
+  try {
+    const container = await new PostgreSqlContainer('postgres:15')
+      .withDatabase('testnet')
+      .withPassword('password')
+      .withUser('postgres')
+      .start()
 
-  const pg = knex({
-    client: 'pg',
-    connection: {
-      user: container.getUsername(),
-      host: container.getHost(),
-      database: container.getDatabase(),
-      port: container.getPort(),
-      password: container.getPassword(),
-      ssl: false
-    },
-    pool: { min: 3, max: 10 },
-    migrations: { directory: path.join(__dirname, '../../migrations') },
-    debug: false
-  })
+    const pg = knex({
+      client: 'pg',
+      connection: {
+        user: container.getUsername(),
+        host: container.getHost(),
+        database: container.getDatabase(),
+        port: container.getPort(),
+        password: container.getPassword(),
+        ssl: false
+      },
+      pool: { min: 3, max: 10 },
+      migrations: { directory: path.join(__dirname, '../migrations') },
+      debug: false
+    })
 
-  // await pg.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-  await pg.migrate.latest()
+    await pg.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
-  Model.knex(pg)
+    await pg.migrate.latest({
+      directory: __dirname + '/../migrations'
+    })
 
-  return { container, pg }
+    Model.knex(pg)
+
+    return { container, pg }
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
 }
 
 export async function stopContainer(
