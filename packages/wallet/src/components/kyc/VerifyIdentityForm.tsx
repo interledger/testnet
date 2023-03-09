a053fbaf9b2ac9b39721f7a3e3e917073bf73c14
Timea <117268143+Tymmmy@users.noreply.github.com>
2023-03-09 15:51:18 +0200

    feat(wallet): Implement requests for wallet creation and verify identity (#144)

--- packages/wallet/src/components/kyc/VerifyIdentityForm.tsx
+++ packages/wallet/src/components/kyc/VerifyIdentityForm.tsx
@@ -1,60 +1,45 @@
+import { userService, verifyIdentitySchema } from '@/lib/api/user'
+import { useDialog } from '@/lib/hooks/useDialog'
+import { useZodForm } from '@/lib/hooks/useZodForm'
 import { Button } from '@/ui/Button'
 import { FieldError } from '@/ui/forms/FieldError'
 import { FileUpload } from '@/ui/forms/FileUpload'
 import { Form } from '@/ui/forms/Form'
-import { useZodForm } from '@/lib/hooks/useZodForm'
+import { getObjectKeys } from '@/utils/helpers'
+import { testImageType, testImageVerifyIdentity } from '@/utils/mocks'
+import { useRouter } from 'next/router'
 import { SyntheticEvent, useState } from 'react'
-import { z } from 'zod'
-
-// mock data, the list will come from Rapyd
-const idTypes = [
-  {
-    id: '1',
-    type: 'id',
-    label: 'ID'
-  },
-  {
-    id: '2',
-    type: 'passport',
-    label: 'Passport'
-  },
-  {
-    id: '3',
-    type: 'driverLicence',
-    label: "Driver's licence"
-  }
-]
-
-const verifyIdentitySchema = z.object({
-  idType: z.string({ invalid_type_error: 'Please select an ID Type' }),
-  frontSideID: z
-    .custom<FileList>()
-    .refine(
-      (frontSideID) => frontSideID?.length === 1,
-      'Front side of ID is required'
-    )
-    .refine(
-      (frontSideID) => frontSideID?.length < 2,
-      'You can only select one image'
-    ),
-  selfie: z
-    .custom<FileList>()
-    .refine((selfie) => selfie?.length === 1, 'Selfie is required')
-    .refine((selfie) => selfie?.length < 2, 'You can only select one image')
-})
+import { ErrorDialog } from '../dialogs/ErrorDialog'
+import { SuccessDialog } from '../dialogs/SuccessDialog'
+import { useKYCFormContext } from './context'
 
 export const VerifyIdentityForm = () => {
+  // set default values and documents for DEV mode
+  const useTestDataKYC = process.env.NEXT_PUBLIC_USE_TEST_KYC_DATA === 'true'
+
+  const [openDialog, closeDialog] = useDialog()
+  const { idTypes } = useKYCFormContext()
   const [frontIDFile, setFrontIDFile] = useState('')
   const [selfieFile, setSelfieFile] = useState('')
+  const [backIDFile, setBackIDFile] = useState('')
+  const [isBackRequired, setIsBackRequired] = useState(false)
+  const router = useRouter()
 
-  const handleOnChange = (event: SyntheticEvent<HTMLInputElement>) => {
+  const handleFileOnChange = (event: SyntheticEvent<HTMLInputElement>) => {
     const target = event.currentTarget
     const fileName = target.value.slice(target.value.lastIndexOf('\\') + 1)
-    if (target.name === 'selfie') {
+    if (target.name === 'faceImage') {
       setSelfieFile(fileName)
-    } else {
+    } else if (target.name === 'frontSideImage') {
       setFrontIDFile(fileName)
+    } else {
+      setBackIDFile(fileName)
     }
+  }
+
+  const handleIdTypeChange = (event: SyntheticEvent<HTMLInputElement>) => {
+    const target = event.currentTarget
+    setIsBackRequired(target.getAttribute('data-back-id') === 'true')
   }
 
   const verifyIdentityForm = useZodForm({
@@ -64,55 +49,161 @@ export const VerifyIdentityForm = () => {
   return (
     <Form
       form={verifyIdentityForm}
-      onSubmit={(data) => {
-        console.log(data)
+      onSubmit={async (data) => {
+        const response = await userService.verifyIdentity(data)
+
+        if (!response) {
+          openDialog(
+            <ErrorDialog
+              onClose={closeDialog}
+              content="Something went wrong. Please try again"
+            />
+          )
+          return
+        }
+
+        if (response.success) {
+          openDialog(
+            <SuccessDialog
+              onClose={() => {
+                closeDialog()
+                router.push('/')
+              }}
+              content="Your identity has been veryfied."
+              redirect="/"
+              redirectText="Go to your account overview"
+            />
+          )
+        } else {
+          const { errors, message } = response
+
+          if (errors) {
+            getObjectKeys(errors).map((field) =>
+              verifyIdentityForm.setError(field, { message: errors[field] })
+            )
+          }
+          if (message) {
+            verifyIdentityForm.setError('root', { message })
+          }
+        }
       }}
     >
+      {useTestDataKYC && (
+        <span className="font-semibold text-pink">
+          For testing purposes Passport is selected, and images uploaded by
+          default!
+        </span>
+      )}
       <div>
-        <div className="flex justify-between">
+        <div className="grid grid-cols-3 gap-3">
           {idTypes.map((idType) => (
-            <div key={idType.id} className="flex w-auto justify-center">
+            <div
+              key={idType.type}
+              className="flex w-auto items-center justify-center"
+            >
               <input
                 type="radio"
-                id={idType.id}
+                id={idType.type}
                 className="peer hidden"
                 value={idType.type}
-                {...verifyIdentityForm.register('idType')}
+                data-back-id={idType.isBackRequired}
+                {...verifyIdentityForm.register('documentType', {
+                  onChange: handleIdTypeChange
+                })}
+                // TESTING => Use Passport as default value
+                checked={useTestDataKYC && idType.type === 'PA'}
+                disabled={useTestDataKYC && idType.type !== 'PA'}
               />
               <label
-                htmlFor={idType.id}
-                className="min-w-[50px] cursor-pointer rounded-xl border border-turqoise bg-white p-2 text-center text-base font-light text-turqoise shadow-md peer-checked:border-orange peer-checked:text-orange sm:min-w-[100px]"
+                htmlFor={idType.type}
+                className="w-[150px] cursor-pointer rounded-xl border border-turqoise bg-white p-2 text-center text-base font-light text-turqoise shadow-md peer-checked:border-orange peer-checked:text-orange sm:min-w-[100px]"
               >
-                {idType.label}
+                {idType.name}
               </label>
             </div>
           ))}
         </div>
         <FieldError
-          error={verifyIdentityForm.formState.errors.idType?.message}
+          error={verifyIdentityForm.formState.errors.documentType?.message}
         />
       </div>
       <div className="flex justify-evenly">
         <div className="my-5">
-          <FileUpload
-            label="Selfie image"
-            {...verifyIdentityForm.register('selfie', {
-              onChange: handleOnChange
-            })}
-            error={verifyIdentityForm.formState.errors.selfie?.message}
-          />
-          <span className="text-sm font-light text-orange">{selfieFile}</span>
+          {!useTestDataKYC && (
+            <>
+              <FileUpload
+                label="Selfie image"
+                {...verifyIdentityForm.register('faceImage', {
+                  onChange: handleFileOnChange
+                })}
+                error={verifyIdentityForm.formState.errors.faceImage?.message}
+              />
+              <span className="text-sm font-light text-orange">
+                {selfieFile}
+              </span>
+            </>
+          )}
+          {useTestDataKYC && (
+            <>
+              <FileUpload disabled={true} label="Selfie image" />
+              <input
+                type="hidden"
+                {...verifyIdentityForm.register('faceImage')}
+                value={testImageVerifyIdentity}
+              />
+              <input
+                type="hidden"
+                {...verifyIdentityForm.register('faceImageType')}
+                value={testImageType}
+              />
+            </>
+          )}
         </div>
         <div className="my-5">
-          <FileUpload
-            label="Front side ID"
-            {...verifyIdentityForm.register('frontSideID', {
-              onChange: handleOnChange
-            })}
-            error={verifyIdentityForm.formState.errors.frontSideID?.message}
-          />
-          <span className="text-sm font-light text-orange">{frontIDFile}</span>
+          {!useTestDataKYC && (
+            <>
+              <FileUpload
+                label="Front side ID"
+                {...verifyIdentityForm.register('frontSideImage', {
+                  onChange: handleFileOnChange
+                })}
+                error={
+                  verifyIdentityForm.formState.errors.frontSideImage?.message
+                }
+              />
+              <span className="text-sm font-light text-orange">
+                {frontIDFile}
+              </span>
+            </>
+          )}
+          {useTestDataKYC && (
+            <>
+              <FileUpload disabled={true} label="Front side ID" />
+              <input
+                type="hidden"
+                {...verifyIdentityForm.register('frontSideImage')}
+                value={testImageVerifyIdentity}
+              />
+              <input
+                type="hidden"
+                {...verifyIdentityForm.register('frontSideImageType')}
+                value={testImageType}
+              />
+            </>
+          )}
         </div>
+        {!useTestDataKYC && isBackRequired && (
+          <div className="my-5">
+            <FileUpload
+              label="Back side ID"
+              {...verifyIdentityForm.register('backSideImage', {
+                onChange: handleFileOnChange
+              })}
+              error={verifyIdentityForm.formState.errors.backSideImage?.message}
+            />
+            <span className="text-sm font-light text-orange">{backIDFile}</span>
+          </div>
+        )}
       </div>
       <Button aria-label="Verify Account" type="submit">
         Verify Account
--- packages/wallet/src/ui/forms/FileUpload.tsx
+++ packages/wallet/src/ui/forms/FileUpload.tsx
@@ -1,6 +1,8 @@
 import { UploadSmile } from '@/components/icons/UploadSmile'
+import { testImageType, testImageVerifyIdentity } from '@/utils/mocks'
 import { ComponentPropsWithoutRef, forwardRef, useId } from 'react'
 import { FieldError } from './FieldError'
+import Image from 'next/image'
 
 type FileUploadProps = Omit<
   ComponentPropsWithoutRef<'input'>,
@@ -13,6 +15,7 @@ type FileUploadProps = Omit<
 export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
   ({ label, error, ...props }, ref) => {
     const id = useId()
+    const useTestDataKYC = process.env.NEXT_PUBLIC_USE_TEST_KYC_DATA === 'true'
 
     return (
       <>
@@ -21,8 +24,19 @@ export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
           className="flex w-24 cursor-pointer flex-col items-center justify-center rounded-xl border-2 border-dashed border-turqoise bg-white text-turqoise hover:border-orange hover:text-orange"
         >
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <UploadSmile />
-            <span className="text-md pt-2 font-light">{label}</span>
+            {!useTestDataKYC ? (
+              <>
+                <UploadSmile />
+                <span className="text-md pt-2 font-light">{label}</span>
+              </>
+            ) : (
+              <Image
+                alt="test"
+                width={100}
+                height={100}
+                src={`data:${testImageType};base64,${testImageVerifyIdentity}`}
+              />
+            )}
           </div>
           <input id={id} ref={ref} type="file" className="hidden" {...props} />
         </label>
