559b3883f825686677cfd5450bc4242b7eeefbd0
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-03-20 08:52:29 +0200

    add knex, objection and migrations

---
+++ packages/backend-update/migrations/20230317135443_create_refresh_tokens.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('refreshTokens', function (table) {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+    table.string('token').notNullable()
+    table.datetime('expiresAt').notNullable()
+
+    table.uuid('userId').notNullable()
+    table.foreign('userId').references('users.id').onDelete('CASCADE')
+
+    table.timestamp('createdAt').notNullable()
+    table.timestamp('updatedAt').notNullable()
+  })
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('refreshTokens')
+}
