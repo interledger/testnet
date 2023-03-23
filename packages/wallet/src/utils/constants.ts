9d06a992712fbd98f1afc5c0c20678f47273cdb9
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-03-23 14:58:30 +0200

    add OP host as constant and fix PP name

--- packages/wallet/src/utils/constants.ts
+++ packages/wallet/src/utils/constants.ts
@@ -3,5 +3,5 @@
  */
 export const USE_TEST_DATA_KYC =
   process.env.NEXT_PUBLIC_USE_TEST_KYC_DATA === 'true'
-
+export const OPEN_PAYMENTS_HOST = process.env.NEXT_PUBLIC_OPEN_PAYMENTS_HOST
 export const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/png']
