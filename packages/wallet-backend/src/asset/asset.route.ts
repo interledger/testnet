31a77f647c4ae3b29f61f89207a4fa3289f04e05
beniaminmunteanu <beniamin.munteanu@xpsol.ro>
2023-04-21 11:26:38 +0300

    remove create asset endpoint remove  leftover linter exclude

--- packages/wallet-backend/src/asset/asset.route.ts
+++ packages/wallet-backend/src/asset/asset.route.ts
@@ -1,15 +1,9 @@
 import express from 'express'
 import passport from 'passport'
-import {
-  create,
-  getById as getAssetById,
-  list as listAsset
-} from './asset.service'
+import { getById as getAssetById, list as listAsset } from './asset.service'
 
 export const assetRouter = express.Router()
 
-//* Testing purpose: In order to properly create assets from postman
-assetRouter.post('', create)
 assetRouter.get('', passport.authenticate('jwt', { session: false }), listAsset)
 assetRouter.get(
   '/:id',
--- packages/wallet-backend/src/rafiki/integration/webhook/webhook.service.ts
+++ packages/wallet-backend/src/rafiki/integration/webhook/webhook.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BadRequestException } from '../../../shared/models/errors/BadRequestException'
 
 import env from '../../../config/env'
