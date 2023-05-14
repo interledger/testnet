ce044ba2d6f32c7181b319ac0a3aebfa9504b0da
beniaminmunteanu <beniamin.munteanu@xpsol.ro>
2023-05-14 16:28:37 +0300

    add rates and quote endpoints for rafiki integration

---
+++ packages/wallet-backend-update/src/rafiki/controller.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from 'express'
+import { Logger } from 'winston'
+import { Quote, RafikiService } from './service'
+
+interface IRafikiController {
+  createQuote: (
+    req: Request,
+    res: CustomResponse<Quote>,
+    next: NextFunction
+  ) => Promise<any>
+  //   getById: ControllerFunction<Asset>
+}
+interface RafikiControllerDependencies {
+  logger: Logger
+  rafikiService: RafikiService
+}
+
+export class RafikiController implements IRafikiController {
+  constructor(private deps: RafikiControllerDependencies) {}
+  public async createQuote(req: Request, res: Response, next: NextFunction) {
+    try {
+      const receivedQuote = req.body
+      const result = await this.deps.rafikiService.createQuote(receivedQuote)
+      return res.status(201).json(result)
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  public async getRates(_req: Request, res: Response, next: NextFunction) {
+    try {
+      res.status(200).json(this.deps.rafikiService.getRates())
+    } catch (e) {
+      next(e)
+    }
+  }
+}
