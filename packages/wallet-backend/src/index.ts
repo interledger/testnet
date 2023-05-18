11d415e5c7949b3d0de271d5abb5adf95308dc15
zainaming <131550590+cryptofan1013@users.noreply.github.com>
2023-05-18 23:35:17 +0500

    cookies domain setting

--- packages/wallet-backend/src/index.ts
+++ packages/wallet-backend/src/index.ts
@@ -34,7 +34,12 @@ app.use(
 )
 app.use(express.json({ limit: '25mb' }))
 app.use(express.urlencoded({ extended: true, limit: '25mb' }))
-app.use(cookieParser())
+// app.use(cookieParser())
+app.use(cookieParser(null, {
+  domain: 'rafiki.money',
+  sameSite: 'none',
+  secure: true
+}));
 
 export const knex = Knex(config[env.NODE_ENV || 'development'])
 
