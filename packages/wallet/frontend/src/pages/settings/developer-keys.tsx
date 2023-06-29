89b54944ec368b77fe68a0e6d8c551415cd3c0db
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-06-29 14:14:16 +0300

    Generate PP key

--- packages/wallet/frontend/src/pages/settings/developer-keys.tsx
+++ packages/wallet/frontend/src/pages/settings/developer-keys.tsx
@@ -1,12 +1,18 @@
+import { ErrorDialog } from '@/components/dialogs/ErrorDialog'
+import { SuccessDialog } from '@/components/dialogs/SuccessDialog'
 import { Chevron } from '@/components/icons/Chevron'
 import { AppLayout } from '@/components/layouts/AppLayout'
 import { PageHeader } from '@/components/PageHeader'
 import { SettingsTabs } from '@/components/SettingsTabs'
+import { Account, accountService } from '@/lib/api/account'
+import { paymentPointerService } from '@/lib/api/paymentPointer'
+import { useDialog } from '@/lib/hooks/useDialog'
 import { NextPageWithLayout } from '@/lib/types/app'
 import { Button } from '@/ui/Button'
 import { CopyButton } from '@/ui/CopyButton'
 import { Disclosure, Transition } from '@headlessui/react'
 import { cx } from 'class-variance-authority'
+import { useRouter } from 'next/router'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next/types'
 
 type DeveloperKeysProps = InferGetServerSidePropsType<typeof getServerSideProps>
@@ -14,6 +20,9 @@ type DeveloperKeysProps = InferGetServerSidePropsType<typeof getServerSideProps>
 const DeveloperKeys: NextPageWithLayout<DeveloperKeysProps> = ({
   accounts
 }) => {
+  const [openDialog, closeDialog] = useDialog()
+  const router = useRouter()
+
   return (
     <>
       <PageHeader title="Developer Keys" />
@@ -70,7 +79,7 @@ const DeveloperKeys: NextPageWithLayout<DeveloperKeysProps> = ({
                                   <div
                                     className={cx(
                                       'h-1.5 w-1.5 rounded-full ring-1',
-                                      paymentPointer.keyId
+                                      paymentPointer.keyIds
                                         ? 'bg-green-4 ring-green-3'
                                         : 'bg-gray-100 ring-gray-300'
                                     )}
@@ -81,20 +90,87 @@ const DeveloperKeys: NextPageWithLayout<DeveloperKeysProps> = ({
                                     {paymentPointer.url}
                                   </p>
                                   <div className="flex-none py-0.5 text-sm leading-5 text-gray-500">
-                                    {paymentPointer.keyId ? (
+                                    {paymentPointer.keyIds ? (
                                       <CopyButton
                                         size="sm"
                                         ctaText="Copy"
                                         afterCtaText="Copied"
-                                        value={paymentPointer.keyId}
+                                        value={paymentPointer.keyIds}
                                         aria-label="copy key id"
                                         className="w-20"
-                                      ></CopyButton>
+                                      />
                                     ) : (
                                       <Button
                                         size="sm"
                                         aria-label="generate key"
                                         className="w-20"
+                                        onClick={async () => {
+                                          const response =
+                                            await paymentPointerService.generateKey(
+                                              {
+                                                accountId:
+                                                  paymentPointer.accountId,
+                                                paymentPointerId:
+                                                  paymentPointer.id
+                                              }
+                                            )
+
+                                          if (!response.success) {
+                                            openDialog(
+                                              <ErrorDialog
+                                                onClose={() => closeDialog()}
+                                                content={response.message}
+                                              />
+                                            )
+                                            return
+                                          }
+
+                                          if (response.data) {
+                                            const { keyId, privateKey } =
+                                              response.data
+
+                                            openDialog(
+                                              <SuccessDialog
+                                                title="Success"
+                                                size="lg"
+                                                content={
+                                                  <>
+                                                    <p>The generated key ID:</p>
+                                                    <code className="text-center">
+                                                      {keyId}
+                                                    </code>
+                                                    <p className="text-xs">
+                                                      (At any time, you can copy
+                                                      the key ID from the
+                                                      Developer Keys section.)
+                                                    </p>
+                                                    <div className="mt-4 space-y-2">
+                                                      <h4 className="text-lg font-bold text-red-400">
+                                                        IMPORTANT!
+                                                      </h4>
+                                                      <p className="text-base">
+                                                        Please ensure to copy
+                                                        the private key and
+                                                        store it securely. This
+                                                        is the only opportunity
+                                                        to make a copy of it.
+                                                      </p>
+                                                      <CopyButton
+                                                        ctaText="COPY PRIVATE KEY"
+                                                        aria-label="copy private key"
+                                                        value={privateKey}
+                                                      />
+                                                    </div>
+                                                  </>
+                                                }
+                                                onClose={() => {
+                                                  closeDialog()
+                                                  router.replace(router.asPath)
+                                                }}
+                                              />
+                                            )
+                                          }
+                                        }}
                                       >
                                         Generate
                                       </Button>
@@ -119,41 +195,30 @@ const DeveloperKeys: NextPageWithLayout<DeveloperKeysProps> = ({
 }
 
 export const getServerSideProps: GetServerSideProps<{
-  accounts: {
-    name: string
-    paymentPointers: {
-      url: string
-      keyId?: string
-    }[]
-  }[]
-}> = async (_ctx) => {
+  accounts: Account[]
+}> = async (ctx) => {
+  const response = await accountService.list(
+    ctx.req.headers.cookie,
+    'paymentPointers'
+  )
+
+  if (!response.success || !response.data) {
+    return {
+      notFound: true
+    }
+  }
+
+  const accounts = response.data.map((account) => ({
+    ...account,
+    paymentPointers: account.paymentPointers.map((pp) => ({
+      ...pp,
+      url: pp.url.replace('https://', '$')
+    }))
+  }))
+
   return {
     props: {
-      accounts: [
-        {
-          name: 'Account #1',
-          paymentPointers: [
-            {
-              url: '$ilp.rafiki.money/pp1',
-              keyId: 'kid1'
-            },
-            {
-              url: '$ilp.rafiki.money/pp2'
-            }
-          ]
-        },
-        {
-          name: 'Account #2',
-          paymentPointers: [
-            {
-              url: '$ilp.rafiki.money/pp3'
-            },
-            {
-              url: '$ilp.rafiki.money/pp4'
-            }
-          ]
-        }
-      ]
+      accounts
     }
   }
 }
