1fc4d39003cba3e1741b9e9781818037bd0043ee
root <andreiii500@yahoo.com>
2023-06-29 15:10:36 +0000

    fix: remove rafikiClient from app.ts

--- packages/wallet/backend/tests/app.ts
+++ packages/wallet/backend/tests/app.ts
@@ -2,11 +2,9 @@ import { start } from '@/index'
 import { App, Bindings } from '@/app'
 import { Container } from '@/shared/container'
 import { Knex } from 'knex'
-import { RafikiClient } from '@/rafiki/rafiki-client'
 
 export interface TestApp {
   knex: Knex
-  rafikiClient: RafikiClient
   port: number
   stop: () => Promise<void>
 }
@@ -16,7 +14,6 @@ export const createApp = async (
 ): Promise<TestApp> => {
   const env = await container.resolve('env')
   const knex = await container.resolve('knex')
-  const rafikiClient = await container.resolve('rafikiClient')
 
   env.PORT = 0
   const app = new App(container)
@@ -24,7 +21,6 @@ export const createApp = async (
 
   return {
     knex,
-    rafikiClient,
     port: app.getPort(),
     stop: app.stop
   }
