410d55a33d745cb7e57e050ea964fca087b25384
Rico Gonzalez <101926344+rico191013@users.noreply.github.com>
2023-06-29 11:26:07 +0300

    feat: developer key backend (#432)
    * moving back register key and generate key apis
    
    * fixed issue in register key api
    
    * added autorization to controllers
    
    * fixed lint issue
    
    * resolved comments
    
    * added patch knex file
    
    * added privatekey, jwk to database
    
    * resolved comments
    
    * resolved comments
    
    * resolved comments
    
    * fixed lint issue
    
    * removed unused console
    
    * merged 2 functions into one
    
    ---------
    
    Co-authored-by: Rico Gonzalez <rico191013>

---
+++ packages/wallet/backend/src/utils/jwk.ts
@@ -0,0 +1,31 @@
+import { Alg, Crv, Kty } from '@/rafiki/generated/graphql'
+import { KeyObject, createPublicKey } from 'crypto'
+
+export const generateJwk = (privateKey: KeyObject, keyId: string) => {
+  if (!keyId.trim()) {
+    throw new Error('KeyId cannot be empty')
+  }
+
+  if (!privateKey) {
+    throw new Error('private key cannot be empty')
+  }
+
+  const jwk = createPublicKey(privateKey).export({
+    format: 'jwk'
+  })
+  if (jwk.x === undefined) {
+    throw new Error('Failed to derive public key')
+  }
+
+  if (jwk.crv !== 'Ed25519' || jwk.kty !== 'OKP' || !jwk.x) {
+    throw new Error('Key is not EdDSA-Ed25519')
+  }
+
+  return {
+    alg: Alg.EdDsa,
+    kid: keyId,
+    kty: Kty.Okp,
+    crv: Crv.Ed25519,
+    x: jwk.x
+  }
+}
