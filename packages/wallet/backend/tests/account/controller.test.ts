fe29357ea3314126932ccd6772c844e784c5f1b0
root <andreiii500@yahoo.com>
2023-07-05 21:09:58 +0000

    test: add listAccounts / getAccountById / fundAccount / withdrawFunds test cases

--- packages/wallet/backend/tests/account/controller.test.ts
+++ packages/wallet/backend/tests/account/controller.test.ts
@@ -4,7 +4,6 @@ import { env } from '@/config/env'
 import { Container } from '@/shared/container'
 import { createApp, TestApp } from '@/tests/app'
 import { Knex } from 'knex'
-import { truncateTables } from '@/tests/tables'
 import { Request, Response } from 'express'
 import {
   createRequest,
@@ -21,6 +20,9 @@ import { Scalars } from '@/rafiki/generated/graphql'
 import { AccountController } from '@/account/controller'
 import { AccountService } from '@/account/service'
 import { User } from '@/user/model'
+import { Account } from '@/account/model'
+import { faker } from '@faker-js/faker'
+import { BaseError } from '@/errors/Base'
 
 describe('Asset Controller', (): void => {
   let bindings: Container<Bindings>
@@ -33,6 +35,8 @@ describe('Asset Controller', (): void => {
   let req: MockRequest<Request>
   let res: MockResponse<Response>
   const mockedAsset = mockedListAssets[0]
+  const accountName = 'Hmmpotato'
+  let createdAccount: Account
 
   const next = jest.fn()
   const args = mockLogInRequest().body
@@ -62,7 +66,16 @@ describe('Asset Controller', (): void => {
             id: 'mocked'
           }
         }),
-        simulateBankTransferToWallet: () => null,
+        simulateBankTransferToWallet: () => ({
+          status: {
+            status: 'SUCCESS'
+          }
+        }),
+        withdrawFundsFromAccount: () => ({
+          status: {
+            status: 'SUCCESS'
+          }
+        }),
         getAccountsBalance: () => ({
           data: [
             {
@@ -79,15 +92,16 @@ describe('Asset Controller', (): void => {
       accountService
     }
     Reflect.set(accountController, 'deps', accountControllerDepsMocked)
+
+    const req = createRequest()
+    req.body = args
+    await userService.create(args)
   })
 
   beforeEach(async (): Promise<void> => {
     res = createResponse()
     req = createRequest()
 
-    req.body = args
-
-    await userService.create(args)
     await applyMiddleware(withSession, req, res)
 
     const { user, session } = await authService.authorize(args)
@@ -106,16 +120,10 @@ describe('Asset Controller', (): void => {
     knex.destroy()
   })
 
-  afterEach(async (): Promise<void> => {
-    await truncateTables(knex)
-  })
-
   describe('createAccount', (): void => {
     it('should create Account for User for a given Asset', async (): Promise<void> => {
-      const name = 'Hmm'
-
       req.body = {
-        name,
+        name: accountName,
         assetId: mockedAsset.id
       }
       await accountController.createAccount(req, res, next)
@@ -123,29 +131,82 @@ describe('Asset Controller', (): void => {
       expect(res._getJSONData()).toMatchObject({
         message: 'SUCCESS',
         data: {
-          name
+          name: accountName
         }
+      })
+      createdAccount = res._getJSONData().data
+    })
+  })
+  describe('listAccounts', (): void => {
+    it("should list User's accounts (1 account, after creation)", async (): Promise<void> => {
+      await accountController.listAccounts(req, res, next)
+      expect(res.statusCode).toBe(200)
+
+      const jsonData = res._getJSONData()
+      expect(jsonData.message).toEqual('SUCCESS')
+      expect(jsonData.data.length).toEqual(1)
+      expect(jsonData.data[0]).toMatchObject({
+        name: accountName
       })
     })
   })
-  // describe('listAccounts', (): void => {
-  //   it("should list User's accounts", async (): Promise<void> => {
-  //     expect(1).toBeTruthy()
-  //   })
-  // })
-  // describe('getAccountById', (): void => {
-  //   it("should get user's Account by id", async (): Promise<void> => {
-  //     expect(1).toBeTruthy()
-  //   })
-  // })
-  // describe('fundAccount', (): void => {
-  //   it("should fund User's Account", async (): Promise<void> => {
-  //     expect(1).toBeTruthy()
-  //   })
-  // })
-  // describe('withdrawFunds', (): void => {
-  //   it("should withdraw from User's Account", async (): Promise<void> => {
-  //     expect(1).toBeTruthy()
-  //   })
-  // })
+
+  describe('getAccountById', (): void => {
+    it("should get User's Account by id", async (): Promise<void> => {
+      req.params = {
+        id: createdAccount.id
+      }
+
+      await accountController.getAccountById(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        message: 'SUCCESS',
+        data: {
+          id: createdAccount.id,
+          name: accountName
+        }
+      })
+    })
+    it("should NOT get User's Account by id, when it's not found", async (): Promise<void> => {
+      req.params = {
+        id: faker.string.uuid()
+      }
+
+      try {
+        await accountController.getAccountById(req, res, next)
+      } catch (err) {
+        expect((err as BaseError).statusCode).toEqual(404)
+      }
+    })
+  })
+  describe('fundAccount', (): void => {
+    it("should fund User's Account", async (): Promise<void> => {
+      req.body = {
+        accountId: createdAccount.id,
+        amount: 5.7
+      }
+
+      await accountController.fundAccount(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Account funded'
+      })
+    })
+  })
+  describe('withdrawFunds', (): void => {
+    it("should withdraw from User's Account", async (): Promise<void> => {
+      req.body = {
+        accountId: createdAccount.id,
+        amount: 3
+      }
+
+      await accountController.withdrawFunds(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Funds withdrawn'
+      })
+    })
+  })
 })
