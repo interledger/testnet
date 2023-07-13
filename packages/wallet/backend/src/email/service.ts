2b1b77d093396e52a66b8761de55b505d4642a2b
Dragos <dragosh1011@gmail.com>
2023-07-13 13:12:24 +0300

    fix: use env variable directly

--- packages/wallet/backend/src/email/service.ts
+++ packages/wallet/backend/src/email/service.ts
@@ -19,14 +19,10 @@ interface EmailServiceDependencies {
 }
 
 export class EmailService implements IEmailService {
-  private readonly from: string
   private readonly baseUrl: string
-  private readonly isSendEmailEnabled: boolean
 
   constructor(private deps: EmailServiceDependencies) {
     sendgrid.setApiKey(this.deps.env.SENDGRID_API_KEY)
-    this.from = this.deps.env.FROM_EMAIL
-    this.isSendEmailEnabled = this.deps.env.SEND_EMAIL
 
     const host = this.deps.env.RAFIKI_MONEY_FRONTEND_HOST
     this.baseUrl =
@@ -40,15 +36,17 @@ export class EmailService implements IEmailService {
   async sendForgotPassword(to: string, token: string): Promise<void> {
     const url = `${this.baseUrl}/auth/reset/${token}`
 
-    this.isSendEmailEnabled
-      ? await this.send({
-          from: this.from,
-          to,
-          subject: '[Rafiki.Money] Reset your password',
-          html: getForgotPasswordEmail(url)
-        })
-      : this.deps.logger.info(
-          `Send email is disabled. Reset password link is: ${url}`
-        )
+    if (this.deps.env.SEND_EMAIL) {
+      return this.send({
+        from: this.deps.env.FROM_EMAIL,
+        to,
+        subject: '[Rafiki.Money] Reset your password',
+        html: getForgotPasswordEmail(url)
+      })
+    }
+
+    this.deps.logger.info(
+      `Send email is disabled. Reset password link is: ${url}`
+    )
   }
 }
