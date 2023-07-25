34f86cee6c9125bb0f0f0a56370c91774d905c2c
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-07-25 07:52:06 +0300

    Add account migration

---
+++ packages/wallet/backend/migrations/20230725043823_update_transactions_accountId_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('transactions', (table) => {
+    table.uuid('accountId').notNullable().alter()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('transactions', (table) => {
+    table.uuid('accountId').nullable().alter()
+  })
+}
