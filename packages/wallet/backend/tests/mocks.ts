b4a17d001b38f8a91b6112e7315064660c59340a
Andrei <andrei.puiu@breakpointit.eu>
2023-08-02 22:36:59 +0000

    test: add test case for 0 transactions + test case for multi-transactions

--- packages/wallet/backend/tests/mocks.ts
+++ packages/wallet/backend/tests/mocks.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { logInSchema, signUpSchema } from '@/auth/validation'
 import z from 'zod'
+import { PartialModelObject } from 'objection'
+import { Transaction } from '../src/transaction/model'
 
 type LogInRequest = z.infer<typeof logInSchema>
 
@@ -47,5 +49,50 @@ export const mockedListAssets = [
     id: 'ca1d9728-d38f-47e6-a88e-3bfe9e60438e',
     scale: 4,
     withdrawalThreshold: null
+  }
+]
+
+export const mockedTransactionInsertObjs: Array<
+  PartialModelObject<Transaction>
+> = [
+  {
+    paymentPointerId: faker.string.uuid(),
+    accountId: faker.string.uuid(),
+    paymentId: faker.string.uuid(),
+    assetCode: mockedListAssets[0].code,
+    value: faker.number.bigInt(),
+    type: 'INCOMING',
+    status: 'PENDING',
+    description: faker.string.alpha(10)
+  },
+  {
+    paymentPointerId: faker.string.uuid(),
+    accountId: faker.string.uuid(),
+    paymentId: faker.string.uuid(),
+    assetCode: mockedListAssets[0].code,
+    value: faker.number.bigInt(),
+    type: 'INCOMING',
+    status: 'PENDING',
+    description: faker.string.alpha(10)
+  },
+  {
+    paymentPointerId: faker.string.uuid(),
+    accountId: faker.string.uuid(),
+    paymentId: faker.string.uuid(),
+    assetCode: mockedListAssets[0].code,
+    value: faker.number.bigInt(),
+    type: 'INCOMING',
+    status: 'PENDING',
+    description: faker.string.alpha(10)
+  },
+  {
+    paymentPointerId: faker.string.uuid(),
+    accountId: faker.string.uuid(),
+    paymentId: faker.string.uuid(),
+    assetCode: mockedListAssets[0].code,
+    value: faker.number.bigInt(),
+    type: 'OUTGOING',
+    status: 'PENDING',
+    description: faker.string.alpha(10)
   }
 ]
