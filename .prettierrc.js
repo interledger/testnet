135b82a4b74795c15da4c4b75b30f3c9b375aad2
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-04 11:05:29 +0300

    chore: fix prettier config (#579)
    Fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,12 +7,6 @@ module.exports = {
   quoteProps: 'consistent',
   jsxSingleQuote: false,
   trailingComma: 'none',
-  /**
-   * This plugin is going automatically sort Tailwind's classes, following
-   * it's recommended class order.
-   *
-   * https://tailwindcss.com/blog/automatic-class-sorting-with-prettier
-   */
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
   tailwindConfig: './packages/wallet/frontend/tailwind.config.js'
 }
