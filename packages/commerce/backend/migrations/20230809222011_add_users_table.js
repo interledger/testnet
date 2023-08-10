ee6d19fd7c63484cffbf42d27f78fab93cd6eaf4
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-10 16:08:01 +0300

    Add migrations

---
+++ packages/commerce/backend/migrations/20230809222011_add_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+    table.string('paymentPointer').notNullable()
+    table.string('token').notNullable()
+
+    table.timestamp('createdAt').notNullable()
+    table.timestamp('updatedAt').notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users')
+}
