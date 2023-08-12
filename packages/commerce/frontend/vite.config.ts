01d9b68b99cabeeeb09d5393b6676f647a79d87b
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-12 15:08:33 +0300

    Add path aliases

--- packages/commerce/frontend/vite.config.ts
+++ packages/commerce/frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
+  }
 })
