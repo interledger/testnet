415064a7f34ce256ac82ed454e604bbba4c375b1
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-17 14:01:37 +0300

    Update product card

--- packages/commerce/frontend/src/components/icons.tsx
+++ packages/commerce/frontend/src/components/icons.tsx
@@ -195,3 +195,23 @@ export const Products = (props: SVGProps<SVGSVGElement>) => {
     </svg>
   )
 }
+
+export const ArrowRightLong = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+      />
+    </svg>
+  )
+}
