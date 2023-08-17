d7ff20e773696b0eb4614ecb5b88a6a704ee067e
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-17 07:25:34 +0300

    Add max width to nav header

--- packages/commerce/frontend/src/components/header.tsx
+++ packages/commerce/frontend/src/components/header.tsx
@@ -5,7 +5,7 @@ import { ShoppingBag } from './icons.tsx'
 export const Header = () => {
   return (
     <header className="supports-backdrop-blur:bg-white/60 sticky top-0 z-50 w-full border-b bg-white backdrop-blur">
-      <div className="flex h-14 items-center justify-between px-8">
+      <div className="mx-auto flex h-14 max-w-[90rem] items-center justify-between px-8">
         <MainNav />
         <div className="flex items-center justify-end">
           <nav className="flex items-center">
