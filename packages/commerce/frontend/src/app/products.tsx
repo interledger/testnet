e66c22d4e29e17890a095e4f05f8daf8b3ef6f62
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-17 07:26:20 +0300

    Add page header

--- packages/commerce/frontend/src/app/products.tsx
+++ packages/commerce/frontend/src/app/products.tsx
@@ -1,3 +1,18 @@
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading
+} from '@/components/page-header'
+
 export function Component() {
-  return <>Products</>
+  return (
+    <>
+      <PageHeader>
+        <PageHeaderHeading>Interledger eCommerce</PageHeaderHeading>
+        <PageHeaderDescription>
+          Value or feeling? Explore all here!
+        </PageHeaderDescription>
+      </PageHeader>
+    </>
+  )
 }
---
+++ packages/commerce/frontend/src/components/page-header.tsx
@@ -0,0 +1,39 @@
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+export const PageHeader = ({
+  className,
+  children,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <section
+      className={cn('flex flex-col items-start text-turqoise', className)}
+      {...props}
+    >
+      {children}
+    </section>
+  )
+}
+
+export const PageHeaderHeading = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLHeadingElement>) => {
+  return (
+    <h1
+      className={cn(
+        'text-xl font-bold leading-tight tracking-tighter md:text-3xl',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export const PageHeaderDescription = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLParagraphElement>) => {
+  return <p className={cn('text-turqoise md:text-lg', className)} {...props} />
+}
