e6d84ec802710d13ad62982474e829949e48c520
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-17 07:36:44 +0300

    Create product card

--- packages/commerce/frontend/src/app/products.tsx
+++ packages/commerce/frontend/src/app/products.tsx
@@ -13,6 +13,9 @@ export function Component() {
           Value or feeling? Explore all here!
         </PageHeaderDescription>
       </PageHeader>
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        Products list
+      </div>
     </>
   )
 }
