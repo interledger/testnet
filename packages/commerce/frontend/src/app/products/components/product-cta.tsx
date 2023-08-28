7549b096edf1dfa5a544625e34c8f5010169524f
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-28 13:12:55 +0300

    Add toast component

--- packages/commerce/frontend/src/app/products/components/product-cta.tsx
+++ packages/commerce/frontend/src/app/products/components/product-cta.tsx
@@ -2,9 +2,25 @@ import { Button } from '@/components/ui/button'
 import { addToCart } from '@/lib/stores/cart-store'
 import { useContext } from 'react'
 import { ProductContext } from '../$slug.tsx'
+import { useToast } from '@/hooks/use-toast.tsx'
 
 export const ProductCTA = () => {
   const { product } = useContext(ProductContext)
+  const { toast } = useToast()
+
+  function handleClick() {
+    addToCart(product),
+      toast({
+        title: 'Item added to cart.',
+        description: (
+          <p>
+            Product <span className="italic">{product.name}</span> was added to
+            cart.
+          </p>
+        ),
+        variant: 'success'
+      })
+  }
 
   return (
     <div className="mt-10">
@@ -12,7 +28,7 @@ export const ProductCTA = () => {
         aria-label="add to cart"
         className="h-12"
         size="lg"
-        onClick={() => addToCart(product)}
+        onClick={() => handleClick()}
       >
         Add to cart
       </Button>
