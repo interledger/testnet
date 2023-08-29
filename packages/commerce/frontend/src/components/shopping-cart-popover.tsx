7fd30d6108b9c6ea5be827b91406f8c58b6f67a1
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-08-29 12:17:46 +0300

    Redirect to cart page an remove custom key handler

--- packages/commerce/frontend/src/components/shopping-cart-popover.tsx
+++ packages/commerce/frontend/src/components/shopping-cart-popover.tsx
@@ -8,23 +8,11 @@ import { useCartStore } from '@/hooks/use-cart-store.ts'
 import { Button } from './ui/button.tsx'
 import { IMAGES_URL } from '@/lib/constants.ts'
 import { PopoverClose } from '@radix-ui/react-popover'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { CartItem } from '@/lib/stores/cart-store.ts'
-import { KeyboardEvent } from 'react'
 
 export const ShoppingCartPopover = () => {
   const { items } = useCartStore()
-  const navigate = useNavigate()
-
-  function handleKeyDown(href: string) {
-    return function (e: KeyboardEvent<HTMLHeadingElement>) {
-      switch (e.key) {
-        case 'Enter':
-          navigate(href)
-          break
-      }
-    }
-  }
 
   return (
     <Popover>
@@ -61,16 +49,17 @@ export const ShoppingCartPopover = () => {
               className="max-h-60 divide-y divide-gray-200 overflow-scroll focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
             >
               {items.map((item) => (
-                <ShoppingCartItem
-                  key={item.id}
-                  item={item}
-                  onKeyDown={handleKeyDown(`/products/${item.slug}`)}
-                />
+                <ShoppingCartItem key={item.id} item={item} />
               ))}
             </ul>
           </div>
-          {/* TODO: Redirect to shopping cart page */}
-          <Button aria-label="go to shopping cart">View shopping cart</Button>
+          <PopoverClose asChild>
+            <Button aria-label="go to shopping cart" asChild>
+              <Link to="/cart" role="button">
+                View shopping cart
+              </Link>
+            </Button>
+          </PopoverClose>
         </div>
       </PopoverContent>
     </Popover>
@@ -79,10 +68,9 @@ export const ShoppingCartPopover = () => {
 
 interface ShoppingCartItemProps {
   item: CartItem
-  onKeyDown: (e: KeyboardEvent<HTMLHeadingElement>) => void
 }
 
-const ShoppingCartItem = ({ item, onKeyDown }: ShoppingCartItemProps) => {
+const ShoppingCartItem = ({ item }: ShoppingCartItemProps) => {
   return (
     <li className="flex items-center py-6 focus-visible:outline-none focus-visible:ring-1">
       <img
@@ -91,14 +79,14 @@ const ShoppingCartItem = ({ item, onKeyDown }: ShoppingCartItemProps) => {
         className="h-12 w-12 flex-none rounded-md border border-gray-200"
       />
       <div className="ml-4 flex-auto">
-        <h3
-          className="inline-block font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-green-3"
-          tabIndex={0}
-          onKeyDown={onKeyDown}
+        <Link
+          to={`/products/${item.slug}`}
+          className="focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-green-3"
         >
-          <Link to={`/products/${item.slug}`}>{item.name}</Link>
+          <h3 className="inline-block font-medium">{item.name}</h3>
           <span className="sr-only">View product {item.name}</span>
-        </h3>
+        </Link>
+
         <p className="text-sm font-thin">Qty {item.quantity}</p>
       </div>
     </li>
