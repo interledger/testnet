bfe86b92c557bb956df316ca3b011805bf894308
Dragos <dragosh1011@gmail.com>
2023-09-07 14:03:48 +0300

    feat(middleware): add handling of axios errors

--- packages/wallet/backend/src/middleware/errorHandler.ts
+++ packages/wallet/backend/src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { logger } from '@/config/logger'
 import { BaseError } from '@/errors/Base'
 import type { Request, NextFunction } from 'express'
+import { AxiosError } from 'axios'
 
 export const errorHandler = (
   e: Error,
@@ -14,6 +15,16 @@ export const errorHandler = (
       message: e.message,
       errors: e.errors
     })
+  }
+  if (e instanceof AxiosError) {
+    logger.debug(JSON.stringify(e.response?.data, undefined, 2))
+
+    const message =
+      typeof e.response?.data.status === 'object'
+        ? e.response?.data.status.message
+        : e.response?.data.message
+    logger.error(message, e.response?.data)
+    res.status(500).json({ success: false, message: 'Internal Server Error' })
   } else {
     logger.error(e)
     res.status(500).json({ success: false, message: 'Internal Server Error' })
