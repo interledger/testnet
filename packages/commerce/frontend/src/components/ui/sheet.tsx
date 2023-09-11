547633c01d781f154e4faaebeee5a61a777b6fef
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2023-09-11 15:03:05 +0300

    lint fixes

--- packages/commerce/frontend/src/components/ui/sheet.tsx
+++ packages/commerce/frontend/src/components/ui/sheet.tsx
@@ -27,7 +27,7 @@ export const SheetOverlay = forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-green-3/60 backdrop-blur-sm',
+      'fixed inset-0 z-50 bg-green-3/60 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
