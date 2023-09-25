e2187a22e5da68634e0a30aa5500ba185e644571
rico191013 <rico.gonzalez001@outlook.com>
2023-09-25 00:44:11 -0700

    removed unused mock tests

--- packages/wallet/backend/tests/helpers.ts
+++ packages/wallet/backend/tests/helpers.ts
@@ -1,28 +1,5 @@
 import { User } from '@/user/model'
-import { createRequest, createResponse } from 'node-mocks-http'
-import { mockedListAssets } from './mocks'
-import { createContainer } from '@/createContainer'
-import { env } from '@/config/env'
 
 export const createUser = (args: Partial<User>) => {
   return User.query().insertAndFetch(args)
-}
-
-export const createMockAccount = async () => {
-  const req = createRequest()
-  const res = createResponse()
-  const accountName = 'mocked-USD'
-  const bindings = createContainer(env)
-  const next = jest.fn()
-
-  req.body = {
-    name: accountName,
-    assetId: mockedListAssets[0].id
-  }
-  const accountController = await bindings.resolve('accountController')
-
-  await accountController.createAccount(req, res, next)
-  const createdAccount = res._getJSONData().data
-
-  return createdAccount
 }
