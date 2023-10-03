08edc08415aafb8f845e8f00d52d4b333518d630
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2023-10-03 09:59:52 +0300

    Restore bad merge from master

--- packages/boutique/frontend/src/components/ui/popover.tsx
+++ packages/boutique/frontend/src/components/ui/popover.tsx
@@ -16,7 +16,7 @@ export const PopoverContent = forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        'data-[side=bottom]:slide-in-from-top-100 z-50 w-72 rounded-md border bg-white p-4 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-100 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border bg-white p-4 shadow-md outline-none',
         className
       )}
       {...props}
--- packages/boutique/frontend/src/components/ui/sheet.tsx
+++ packages/boutique/frontend/src/components/ui/sheet.tsx
@@ -30,7 +30,7 @@ export const SheetOverlay = forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-green-3/60 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-green-3/60 backdrop-blur-sm',
       className
     )}
     {...props}
