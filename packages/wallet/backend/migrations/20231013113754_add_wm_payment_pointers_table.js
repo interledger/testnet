35ff20387538b2720f024f071aff590980436d97
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-10-13 14:56:09 +0300

    Add migrations

---
+++ packages/wallet/backend/migrations/20231013113754_add_wm_payment_pointers_table.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('wmPaymentPointers', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+    table.string('url').notNullable()
+    table.string('publicName').notNullable()
+
+    table.uuid('accountId').notNullable()
+    table.foreign('accountId').references('accounts.id').onDelete('CASCADE')
+    table.boolean('active').notNullable().defaultTo(true)
+    table.string('assetCode', 3).notNullable()
+    table.smallint('assetScale').notNullable()
+    table.bigInteger('balance').notNullable().defaultTo(0)
+    table.jsonb('keyIds')
+
+    table.timestamp('createdAt').notNullable()
+    table.timestamp('updatedAt').notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('wmPaymentPointers')
+}
