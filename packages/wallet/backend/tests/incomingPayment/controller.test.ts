a22d69b30c9419a707594a4e98a8d2c3d62845bc
Mário Varela <mariovarela99@outlook.com>
2023-10-24 16:31:53 +0000

    style: add new lines and organize imports

--- packages/wallet/backend/tests/incomingPayment/controller.test.ts
+++ packages/wallet/backend/tests/incomingPayment/controller.test.ts
@@ -1,20 +1,20 @@
 import { env } from '@/config/env'
-import {
-  createRequest,
-  createResponse,
-  MockRequest,
-  MockResponse
-} from 'node-mocks-http'
-import { NextFunction, Request, Response } from 'express'
 import { createContainer } from '@/createContainer'
 import { IncomingPaymentController } from '@/incomingPayment/controller'
+import { errorHandler } from '@/middleware/errorHandler'
+import { NextFunction, Request, Response } from 'express'
+import {
+  MockRequest,
+  MockResponse,
+  createRequest,
+  createResponse
+} from 'node-mocks-http'
 import {
   mockIncomingPaymentGetPaymentDetailsByUrlRequest,
   mockIncomingPaymentRequest,
   mockIncomingPaymentRequestSession as mockIncomingPaymentRequestSessionUser,
   mockIncomingPaymentService
 } from '../mocks'
-import { errorHandler } from '@/middleware/errorHandler'
 
 describe('Incoming Payment Controller', () => {
   let req: MockRequest<Request>
@@ -52,7 +52,9 @@ describe('Incoming Payment Controller', () => {
     it('should call method create() in incomingPaymentService', async () => {
       const createSpy = jest.spyOn(mockIncomingPaymentService, 'create')
       req.body = mockIncomingPaymentRequest().body
+
       await incomingPaymentController.create(req, res, next)
+
       expect(createSpy).toHaveBeenCalled()
       expect(createSpy).toHaveBeenCalledTimes(1)
       expect(createSpy).toHaveBeenCalledWith(
@@ -63,9 +65,12 @@ describe('Incoming Payment Controller', () => {
         req.body.expiration
       )
     })
+
     it("should return status 200 and message 'SUCCESS' if the payment was created", async () => {
       req.body = mockIncomingPaymentRequest().body
+
       await incomingPaymentController.create(req, res, next)
+
       expect(res.statusCode).toBe(200)
       expect(res._getJSONData()).toMatchObject({
         success: true,
@@ -73,23 +78,29 @@ describe('Incoming Payment Controller', () => {
         data: { url: 'https://www.some-domain.com' }
       })
     })
+
     it('should return status 400 if the incomingPayment body is not valid', async () => {
       req.body = mockIncomingPaymentRequest().body
       delete req.body.paymentPointerId
+
       await incomingPaymentController.create(req, res, (err) => {
         next()
         errorHandler(err, req, res, next)
       })
+
       expect(res.statusCode).toBe(400)
     })
+
     it('should not call method create() in incomingPaymentService if the incomingPayment body is not valid', async () => {
       const createSpy = jest.spyOn(mockIncomingPaymentService, 'create')
       req.body = mockIncomingPaymentRequest().body
       delete req.body.paymentPointerId
+
       await incomingPaymentController.create(req, res, (err) => {
         next()
         errorHandler(err, req, res, next)
       })
+
       expect(createSpy).not.toBeCalled()
     })
 
@@ -100,24 +111,31 @@ describe('Incoming Payment Controller', () => {
           'getPaymentDetailsByUrl'
         )
         req.query = mockIncomingPaymentGetPaymentDetailsByUrlRequest().query
+
         await incomingPaymentController.getPaymentDetailsByUrl(req, res, next)
+
         expect(getPaymentDetailsByUrlSpy).toHaveBeenCalled()
         expect(getPaymentDetailsByUrlSpy).toHaveBeenCalledTimes(1)
         expect(getPaymentDetailsByUrlSpy).toHaveBeenCalledWith(req.query.url)
       })
+
       it("should return status 200 and message 'SUCCESS' if the payment details were fetched", async () => {
         req.query = mockIncomingPaymentGetPaymentDetailsByUrlRequest().query
+
         await incomingPaymentController.getPaymentDetailsByUrl(req, res, next)
+
         expect(res.statusCode).toBe(200)
         expect(res._getJSONData()).toMatchObject({
           success: true,
           message: 'SUCCESS'
         })
       })
+
       it('should return status 400 if the incomingPayment body is not valid', async () => {
         req.query = mockIncomingPaymentGetPaymentDetailsByUrlRequest({
           url: 'some-url'
         }).query
+
         await incomingPaymentController.getPaymentDetailsByUrl(
           req,
           res,
@@ -126,12 +144,14 @@ describe('Incoming Payment Controller', () => {
             errorHandler(err, req, res, next)
           }
         )
+
         expect(res.statusCode).toBe(400)
         expect(res._getJSONData()).toMatchObject({
           success: false,
           message: 'Invalid input'
         })
       })
+
       it('should not call method getPaymentDetailsByUrl() in incomingPaymentService if the incomingPayment body is not valid', async () => {
         const getPaymentDetailsByUrlSpy = jest.spyOn(
           mockIncomingPaymentService,
@@ -140,6 +160,7 @@ describe('Incoming Payment Controller', () => {
         req.query = mockIncomingPaymentGetPaymentDetailsByUrlRequest({
           url: 'some-url'
         }).query
+
         await incomingPaymentController.getPaymentDetailsByUrl(
           req,
           res,
@@ -148,6 +169,7 @@ describe('Incoming Payment Controller', () => {
             errorHandler(err, req, res, next)
           }
         )
+
         expect(getPaymentDetailsByUrlSpy).not.toBeCalled()
       })
     })
