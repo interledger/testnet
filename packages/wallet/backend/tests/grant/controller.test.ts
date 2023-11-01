f4091cb6fa2b6d422454f13cbc84aeb0509feec7
Rico <rico.gonzalez001@outlook.com>
2023-11-01 20:57:04 +0330

    added unit tests for Grant service

--- packages/wallet/backend/tests/grant/controller.test.ts
+++ packages/wallet/backend/tests/grant/controller.test.ts
@@ -1,169 +1,184 @@
-import {Container} from "@/shared/container";
-import {Bindings} from "@/app";
-import {createApp, TestApp} from "@/tests/app";
-import {Knex} from "knex";
-import {AuthService} from "@/auth/service";
-import {createRequest, createResponse, MockRequest, MockResponse} from "node-mocks-http";
-import {Request, Response} from "express";
-import {mockLogInRequest} from "@/tests/mocks";
-import {GrantController} from "@/grant/controller";
-import {applyMiddleware} from "@/tests/utils";
-import {withSession} from "@/middleware/withSession";
-import {User} from "@/user/model";
-import {faker} from "@faker-js/faker";
-import {createContainer} from "@/createContainer";
-import {env} from "@/config/env";
-import {createUser} from "@/tests/helpers";
-import {truncateTables} from "@/tests/tables";
-import {errorHandler} from "@/middleware/errorHandler";
-import {describe} from "node:test";
+import { Container } from '@/shared/container'
+import { Bindings } from '@/app'
+import { createApp, TestApp } from '@/tests/app'
+import { Knex } from 'knex'
+import { AuthService } from '@/auth/service'
+import {
+  createRequest,
+  createResponse,
+  MockRequest,
+  MockResponse
+} from 'node-mocks-http'
+import { Request, Response } from 'express'
+import { mockLogInRequest } from '@/tests/mocks'
+import { GrantController } from '@/grant/controller'
+import { applyMiddleware } from '@/tests/utils'
+import { withSession } from '@/middleware/withSession'
+import { User } from '@/user/model'
+import { faker } from '@faker-js/faker'
+import { createContainer } from '@/createContainer'
+import { env } from '@/config/env'
+import { createUser } from '@/tests/helpers'
+import { truncateTables } from '@/tests/tables'
+import { errorHandler } from '@/middleware/errorHandler'
 
-describe('Grant Controller',()=>{
-    let bindings: Container<Bindings>
-    let appContainer: TestApp
-    let knex: Knex
-    let authService: AuthService
-    let grantController: GrantController
-    let req: MockRequest<Request>
-    let res: MockResponse<Response>
+describe('Grant Controller', () => {
+  let bindings: Container<Bindings>
+  let appContainer: TestApp
+  let knex: Knex
+  let authService: AuthService
+  let grantController: GrantController
+  let req: MockRequest<Request>
+  let res: MockResponse<Response>
 
-    const next = jest.fn()
-    const args = mockLogInRequest().body
+  const next = jest.fn()
+  const args = mockLogInRequest().body
 
-    const createReqRes = async () => {
-        res = createResponse()
-        req = createRequest()
+  const createReqRes = async () => {
+    res = createResponse()
+    req = createRequest()
 
-        await applyMiddleware(withSession, req, res)
+    await applyMiddleware(withSession, req, res)
 
-        const { user, session } = await authService.authorize(args)
-        req.session.id = session.id
-        req.session.user = {
-            id: user.id,
-            email: user.email,
-            needsWallet: !user.rapydWalletId,
-            needsIDProof: !user.kycId
-        }
-        await User.query().patchAndFetchById(user.id, { rapydWalletId: 'mocked' })
+    const { user, session } = await authService.authorize(args)
+    req.session.id = session.id
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      needsWallet: !user.rapydWalletId,
+      needsIDProof: !user.kycId
     }
+    await User.query().patchAndFetchById(user.id, { rapydWalletId: 'mocked' })
+  }
 
-    const createMockGrantControllerDeps = (isFailure?: boolean) => {
-        const grantControllerDepsMocked = {
-            rafikiAuthService: {
-                listGrants:jest.fn().mockReturnValue(['grant1']),
-                revokeGrant: isFailure? jest.fn().mockRejectedValueOnce(new Error('Unexpected error')) : jest.fn(),
-                getGrantById:jest.fn().mockReturnValue({id: 'grant'}),
-            },
-            grantService:{
-                getGrantByInteraction:jest.fn().mockReturnValue({id: 'grant'}),
-                setInteractionResponse:jest.fn().mockReturnValue({id: 'grant'}),
-            },
-            paymentPointerService:{
-                listIdentifiersByUserId:()=> faker.lorem.words(5).split(' ')
-            }
-        }
-        Reflect.set(grantController, 'deps', grantControllerDepsMocked)
+  const createMockGrantControllerDeps = (isFailure?: boolean) => {
+    const grantControllerDepsMocked = {
+      rafikiAuthService: {
+        listGrants: jest.fn().mockReturnValue(['grant1']),
+        revokeGrant: isFailure
+          ? jest.fn().mockRejectedValueOnce(new Error('Unexpected error'))
+          : jest.fn(),
+        getGrantById: jest.fn().mockReturnValue({ id: 'grant' })
+      },
+      grantService: {
+        getGrantByInteraction: jest.fn().mockReturnValue({ id: 'grant' }),
+        setInteractionResponse: jest.fn().mockReturnValue({ id: 'grant' })
+      },
+      paymentPointerService: {
+        listIdentifiersByUserId: () => faker.lorem.words(5).split(' ')
+      }
     }
+    Reflect.set(grantController, 'deps', grantControllerDepsMocked)
+  }
 
-    beforeAll(async (): Promise<void> => {
-        bindings = createContainer(env)
-        appContainer = await createApp(bindings)
-        knex = appContainer.knex
-        authService = await bindings.resolve('authService')
-        grantController = await bindings.resolve('grantController')
+  beforeAll(async (): Promise<void> => {
+    bindings = createContainer(env)
+    appContainer = await createApp(bindings)
+    knex = appContainer.knex
+    authService = await bindings.resolve('authService')
+    grantController = await bindings.resolve('grantController')
 
-        createMockGrantControllerDeps()
+    createMockGrantControllerDeps()
+  })
+
+  beforeEach(async (): Promise<void> => {
+    await createUser({ ...args, isEmailVerified: true })
+    await createReqRes()
+  })
+
+  afterAll(async (): Promise<void> => {
+    await appContainer.stop()
+    await knex.destroy()
+  })
+
+  afterEach(async (): Promise<void> => {
+    await truncateTables(knex)
+    createMockGrantControllerDeps()
+  })
+
+  describe('Get list of grants', () => {
+    it('should return array', async () => {
+      await grantController.list(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Success',
+        data: ['grant1']
+      })
+    })
+  })
+
+  describe('Revoke Grant', () => {
+    it('should return undefined', async () => {
+      req.params = {
+        id: faker.string.uuid()
+      }
+      await grantController.revoke(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Success'
+      })
     })
 
-    beforeEach(async (): Promise<void> => {
-        await createUser({ ...args, isEmailVerified: true })
-        await createReqRes()
+    it('should fail with status 500 on unexpected error', async () => {
+      createMockGrantControllerDeps(true)
+      req.params = {
+        id: faker.string.uuid()
+      }
+      await grantController.revoke(req, res, (err) => {
+        next()
+        errorHandler(err, req, res, next)
+      })
+      expect(next).toHaveBeenCalledTimes(1)
+      expect(res.statusCode).toBe(500)
+      expect(res._getJSONData()).toMatchObject({
+        success: false,
+        message: 'Internal Server Error'
+      })
     })
+  })
 
-    afterAll(async (): Promise<void> => {
-        await appContainer.stop()
-        await knex.destroy()
+  describe('Get By Id', () => {
+    it('should return a grant', async () => {
+      await grantController.getById(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Success',
+        data: { id: 'grant' }
+      })
     })
+  })
 
-    afterEach(async (): Promise<void> => {
-        await truncateTables(knex)
-        createMockGrantControllerDeps()
+  describe('Get By Interaction', () => {
+    it('should return a grant', async () => {
+      req.params = {
+        interactionId: faker.string.uuid(),
+        nonce: faker.string.uuid()
+      }
+      await grantController.getByInteraction(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Success',
+        data: { id: 'grant' }
+      })
     })
+  })
 
-    describe('Get list of grants', ()=>{
-        it('should return array', async ()=>{
-            await grantController.list(req,res, next)
-            expect(res.statusCode).toBe(200)
-            expect(res._getJSONData()).toMatchObject({
-                success:true, message:'Success', data:['grant1']
-            })
-        })
+  describe('Set Interaction Response', () => {
+    it('should return a grant', async () => {
+      req.body = {
+        response: 'accept'
+      }
+      await grantController.setInteractionResponse(req, res, next)
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Success',
+        data: { id: 'grant' }
+      })
     })
-
-    describe('Revoke Grant',()=>{
-        it('should return undefined',async ()=>{
-            req.params={
-                id:faker.string.uuid()
-            }
-            await grantController.revoke(req,res, next)
-            expect(res.statusCode).toBe(200)
-            expect(res._getJSONData()).toMatchObject({
-                success:true, message:'Success'
-            })
-        })
-
-        it('should fail with status 500 on unexpected error',async ()=>{
-            createMockGrantControllerDeps(true)
-            req.params={
-                id:faker.string.uuid()
-            }
-            await grantController.revoke(req, res, (err) => {
-                next()
-                errorHandler(err, req, res, next)
-            })
-            expect(next).toHaveBeenCalledTimes(1)
-            expect(res.statusCode).toBe(500)
-            expect(res._getJSONData()).toMatchObject({
-                success: false,
-                message: 'Internal Server Error'
-            })
-        })
-    })
-
-    describe('Get By Id',()=>{
-        it('should return a grant', async ()=>{
-            await grantController.getById(req,res, next)
-            expect(res.statusCode).toBe(200)
-            expect(res._getJSONData()).toMatchObject({
-                success:true, message:'Success', data:{id:'grant'}
-            })
-        })
-    })
-
-    describe('Get By Interaction',()=>{
-        it('should return a grant', async ()=>{
-            req.params={
-                interactionId:faker.string.uuid(),
-                nonce:faker.string.uuid(),
-            }
-            await grantController.getByInteraction(req,res, next)
-            expect(res.statusCode).toBe(200)
-            expect(res._getJSONData()).toMatchObject({
-                success:true, message:'Success', data:{id:'grant'}
-            })
-        })
-    })
-
-    describe('Set Interaction Response',()=>{
-        it('should return a grant', async ()=>{
-            req.body ={
-                response:'accept'
-            }
-            await grantController.setInteractionResponse(req,res, next)
-            expect(res.statusCode).toBe(200)
-            expect(res._getJSONData()).toMatchObject({
-                success:true, message:'Success', data:{id:'grant'}
-            })
-        })
-    })
+  })
 })
---
+++ packages/wallet/backend/tests/grant/service.test.ts
@@ -0,0 +1,113 @@
+import { Container } from '@/shared/container'
+import { Bindings } from '@/app'
+import { createApp, TestApp } from '@/tests/app'
+import { Knex } from 'knex'
+import { AuthService } from '@/auth/service'
+import { GrantService } from '@/grant/service'
+import { createContainer } from '@/createContainer'
+import { env } from '@/config/env'
+import { loginUser } from '@/tests/utils'
+import { faker } from '@faker-js/faker'
+import { truncateTables } from '@/tests/tables'
+import { Forbidden } from '@/errors'
+
+describe('Grant Service', () => {
+  let bindings: Container<Bindings>
+  let appContainer: TestApp
+  let knex: Knex
+  let authService: AuthService
+  let grantService: GrantService
+  let userId: string
+
+  beforeAll(async (): Promise<void> => {
+    bindings = createContainer(env)
+    appContainer = await createApp(bindings)
+    knex = appContainer.knex
+    authService = await bindings.resolve('authService')
+    grantService = await bindings.resolve('grantService')
+
+    createMockGrantServiceDeps()
+  })
+
+  const createMockGrantServiceDeps = (isFailure?: boolean) => {
+    const grantServiceDepsMocked = {
+      paymentPointerService: {
+        belongsToUser: () => !isFailure
+      },
+      rafikiAuthService: {
+        setInteractionResponse: jest.fn().mockReturnValue({ id: 'grant' }),
+        revokeGrant: isFailure
+          ? jest.fn().mockRejectedValueOnce(new Error('Unexpected error'))
+          : jest.fn(),
+        getGrantByInteraction: jest
+          .fn()
+          .mockReturnValue({
+            id: 'grant',
+            access: [{ identifier: faker.internet.url() }]
+          })
+      }
+    }
+
+    Reflect.set(grantService, 'deps', grantServiceDepsMocked)
+  }
+
+  beforeEach(async (): Promise<void> => {
+    const extraUserArgs = {
+      isEmailVerified: true,
+      rapydWalletId: 'mocked'
+    }
+
+    const { user } = await loginUser({
+      authService,
+      extraUserArgs
+    })
+    userId = user.id
+  })
+
+  afterAll(async (): Promise<void> => {
+    await appContainer.stop()
+    await knex.destroy()
+  })
+
+  afterEach(async (): Promise<void> => {
+    await truncateTables(knex)
+    createMockGrantServiceDeps()
+  })
+
+  describe('Get Grant By Interaction', () => {
+    it('should return a grant', async () => {
+      const result = await grantService.getGrantByInteraction(
+        userId,
+        'test1',
+        'test2'
+      )
+      expect(result).toMatchObject({
+        id: 'grant'
+      })
+      expect(result).toHaveProperty('access')
+      expect(result.access).toHaveLength(1)
+      expect(result.access[0]).toHaveProperty('identifier')
+    })
+
+    it('should return no access err', async () => {
+      createMockGrantServiceDeps(true)
+      await expect(
+        grantService.getGrantByInteraction(userId, 'test1', 'test2')
+      ).rejects.toThrowError(new Forbidden('NO_ACCESS'))
+    })
+  })
+
+  describe('set Interaction Response', () => {
+    it('should return a grant', async () => {
+      const result = await grantService.setInteractionResponse(
+        userId,
+        'test1',
+        'test2',
+        'accept'
+      )
+      expect(result).toMatchObject({
+        id: 'grant'
+      })
+    })
+  })
+})
