24629cb92552e68f4dabfc99e504627437e613d3
Rico <rico.gonzalez001@outlook.com>
2024-01-23 21:48:33 +0330

    test labels check 2

--- packages/wallet/backend/src/app.ts
+++ packages/wallet/backend/src/app.ts
@@ -89,7 +89,7 @@ export class App {
   constructor(private container: AwilixContainer<Cradle>) {}
 
   public async startServer(): Promise<void> {
-    console.log('test')
+    console.log('test2')
     const express = await this.init()
     const env = await this.container.resolve('env')
     const logger = await this.container.resolve('logger')
