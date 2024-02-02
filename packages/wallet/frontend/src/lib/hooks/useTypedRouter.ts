8a950d9c6d098233185dfa41612b0b10211e16f0
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-02-02 12:56:41 +0200

    Update useTypedRouter.ts

--- packages/wallet/frontend/src/lib/hooks/useTypedRouter.ts
+++ packages/wallet/frontend/src/lib/hooks/useTypedRouter.ts
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import { ZodSchema, z } from 'zod'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useTypedRouter: any = <T extends ZodSchema>(schema: T) => {
   const { query, ...router } = useRouter()
 
