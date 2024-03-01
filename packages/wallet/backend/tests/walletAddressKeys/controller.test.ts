2a7f7bde85241832e849e5a0c0008bb093cc787c
dragosp1011 <109967337+dragosp1011@users.noreply.github.com>
2024-03-01 11:13:56 +0200

    test: add developer keys tests (#1171)
    * test: add developer keys tests
    
    * test: update names

---
+++ packages/wallet/backend/tests/walletAddressKeys/controller.test.ts
@@ -0,0 +1,251 @@
+import { createApp, TestApp } from '@/tests/app'
+import { Knex } from 'knex'
+import { AuthService } from '@/auth/service'
+import { AwilixContainer } from 'awilix'
+import {
+  createRequest,
+  createResponse,
+  MockRequest,
+  MockResponse
+} from 'node-mocks-http'
+import { Request, Response } from 'express'
+import { mockLogInRequest } from '@/tests/mocks'
+import { applyMiddleware } from '@/tests/utils'
+import { withSession } from '@/middleware/withSession'
+import { User } from '@/user/model'
+import { Cradle, createContainer } from '@/createContainer'
+import { env } from '@/config/env'
+import { createUser } from '@/tests/helpers'
+import { truncateTables } from '@/tests/tables'
+import { faker } from '@faker-js/faker'
+import { WalletAddressKeyController } from '@/walletAddressKeys/controller'
+import { WalletAddressKeyService } from '@/walletAddressKeys/service'
+
+describe('Wallet Address Keys Controller', () => {
+  let bindings: AwilixContainer<Cradle>
+  let appContainer: TestApp
+  let knex: Knex
+  let authService: AuthService
+  let walletAddressKeyController: WalletAddressKeyController
+  let req: MockRequest<Request>
+  let res: MockResponse<Response>
+  let userId: string
+  let walletAddressKeyServiceMock: Partial<WalletAddressKeyService>
+  const keyResponse = {
+    privateKey: faker.lorem.slug(5),
+    publicKey: faker.lorem.slug(5),
+    keyId: faker.string.uuid(),
+    nickname: faker.lorem.word()
+  }
+
+  const next = jest.fn()
+  const args = mockLogInRequest().body
+
+  const createReqRes = async () => {
+    res = createResponse()
+    req = createRequest()
+
+    await applyMiddleware(withSession, req, res)
+
+    const { user, session } = await authService.authorize(args)
+    req.session.id = session.id
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      needsWallet: !user.rapydWalletId,
+      needsIDProof: !user.kycId
+    }
+    userId = user.id
+    await User.query().patchAndFetchById(user.id, { rapydWalletId: 'mocked' })
+  }
+
+  const createMockWalletAddressKeyControllerDeps = () => {
+    walletAddressKeyServiceMock = {
+      revokeKey: () => Promise.resolve(),
+      uploadKey: () => Promise.resolve(),
+      patch: () => Promise.resolve(),
+      registerKey: () => Promise.resolve(keyResponse),
+      listByWalletId: () => Promise.resolve([])
+    }
+    Reflect.set(
+      walletAddressKeyController,
+      'walletAddressKeyService',
+      walletAddressKeyServiceMock
+    )
+  }
+
+  beforeAll(async (): Promise<void> => {
+    bindings = await createContainer(env)
+    appContainer = await createApp(bindings)
+    knex = appContainer.knex
+    authService = await bindings.resolve('authService')
+    walletAddressKeyController = await bindings.resolve(
+      'walletAddressKeyController'
+    )
+
+    createMockWalletAddressKeyControllerDeps()
+  })
+
+  beforeEach(async (): Promise<void> => {
+    await createUser({ ...args, isEmailVerified: true })
+    await createReqRes()
+  })
+
+  afterAll(async (): Promise<void> => {
+    await appContainer.stop()
+    await knex.destroy()
+  })
+
+  afterEach(async (): Promise<void> => {
+    await truncateTables(knex)
+  })
+
+  describe('List', () => {
+    it('should call listByWalletId', async () => {
+      const listSpy = jest.spyOn(walletAddressKeyServiceMock, 'listByWalletId')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid()
+      }
+
+      await walletAddressKeyController.list(req, res, next)
+      expect(listSpy).toHaveBeenCalledWith({ userId, ...req.params })
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'SUCCESS'
+      })
+      expect(res._getJSONData()).toHaveProperty('result')
+      expect(res._getJSONData().result).toStrictEqual([])
+    })
+  })
+
+  describe('Patch', () => {
+    it('should call patch', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'patch')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid(),
+        keyId: faker.string.uuid()
+      }
+
+      req.body = {
+        nickname: faker.lorem.word()
+      }
+
+      await walletAddressKeyController.patchKey(req, res, next)
+      expect(spy).toHaveBeenCalledWith({ userId, ...req.params, ...req.body })
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Public key nickname updated.'
+      })
+    })
+
+    it('should fail nickname validation', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'patch')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid(),
+        keyId: faker.string.uuid()
+      }
+
+      req.body = { nickname: 1 }
+
+      await walletAddressKeyController.patchKey(req, res, next)
+      expect(spy).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('Register key', () => {
+    it('should call registerKey', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'registerKey')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid()
+      }
+
+      req.body = {
+        nickname: faker.lorem.word()
+      }
+
+      await walletAddressKeyController.registerKey(req, res, next)
+      expect(spy).toHaveBeenCalledWith({ userId, ...req.params, ...req.body })
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Public key is successfully registered.'
+      })
+      expect(res._getJSONData().result).toMatchObject(keyResponse)
+    })
+
+    it('should fail nickname validation', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'registerKey')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid(),
+        keyId: faker.string.uuid()
+      }
+
+      req.body = { nickname: 1 }
+
+      await walletAddressKeyController.registerKey(req, res, next)
+      expect(spy).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('Upload key', () => {
+    it('should call uploadKey', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'uploadKey')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid()
+      }
+
+      req.body = {
+        nickname: faker.lorem.word(),
+        base64Key: faker.lorem.word()
+      }
+
+      await walletAddressKeyController.uploadKey(req, res, next)
+      expect(spy).toHaveBeenCalledWith({ userId, ...req.params, ...req.body })
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Public key is successfully uploaded.'
+      })
+    })
+
+    it('should fail validation', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'uploadKey')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid()
+      }
+
+      req.body = {}
+
+      await walletAddressKeyController.uploadKey(req, res, next)
+      expect(spy).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('Revoke key', () => {
+    it('should call revokeKey', async () => {
+      const spy = jest.spyOn(walletAddressKeyServiceMock, 'revokeKey')
+      req.params = {
+        accountId: faker.string.uuid(),
+        walletAddressId: faker.string.uuid(),
+        keyId: faker.string.uuid()
+      }
+
+      await walletAddressKeyController.revokeKey(req, res, next)
+      expect(spy).toHaveBeenCalledWith({ userId, ...req.params, ...req.body })
+      expect(res.statusCode).toBe(200)
+      expect(res._getJSONData()).toMatchObject({
+        success: true,
+        message: 'Public key was successfully revoked.'
+      })
+    })
+  })
+})
---
+++ packages/wallet/backend/tests/walletAddressKeys/service.test.ts
@@ -0,0 +1,291 @@
+import { createApp, TestApp } from '@/tests/app'
+import { Knex } from 'knex'
+import { faker } from '@faker-js/faker'
+import { Cradle, createContainer } from '@/createContainer'
+import { env } from '@/config/env'
+import { truncateTables } from '@/tests/tables'
+import { AwilixContainer } from 'awilix'
+import { WalletAddressKeyService } from '@/walletAddressKeys/service'
+import { WalletAddressKeys } from '@/walletAddressKeys/model'
+import { Account } from '@/account/model'
+import { mockedListAssets } from '@/tests/mocks'
+import { WalletAddress } from '@/walletAddress/model'
+import { loginUser } from '@/tests/utils'
+import { AuthService } from '@/auth/service'
+import { NotFound } from '@/errors'
+
+describe('Wallet Address Key Service', () => {
+  let bindings: AwilixContainer<Cradle>
+  let appContainer: TestApp
+  let knex: Knex
+  let walletAddressKeyService: WalletAddressKeyService
+  let authService: AuthService
+  let userId: string
+  let rafikiClientMock: Record<string, () => unknown>
+
+  const rafikiKeyId = faker.string.uuid()
+  const walletAddress = {
+    url: `https://test/${faker.lorem.slug()}`,
+    publicName: faker.string.alpha(10),
+    accountId: faker.string.uuid(),
+    id: faker.string.uuid()
+  }
+
+  const prepareRelatedData = async () => {
+    const extraUserArgs = {
+      isEmailVerified: true,
+      rapydWalletId: 'mocked'
+    }
+
+    const { user } = await loginUser({
+      authService,
+      extraUserArgs
+    })
+    userId = user.id
+
+    await Account.query().insert({
+      id: walletAddress.accountId,
+      name: faker.string.alpha(10),
+      userId,
+      assetCode: mockedListAssets[0].code,
+      assetId: mockedListAssets[0].id,
+      assetScale: mockedListAssets[0].scale,
+      virtualAccountId: 'mocked'
+    })
+
+    await WalletAddress.query().insert(walletAddress)
+  }
+
+  function prepareWSKDepsMock() {
+    rafikiClientMock = {
+      createRafikiWalletAddressKey: () => ({
+        id: rafikiKeyId
+      }),
+      revokeWalletAddressKey: jest.fn()
+    }
+    const wakServiceDepsMocked = {
+      walletAddressService: {
+        getById: () => Promise.resolve(walletAddress)
+      },
+      rafikiClient: rafikiClientMock
+    }
+
+    for (const key in wakServiceDepsMocked)
+      Reflect.set(
+        walletAddressKeyService,
+        key,
+        wakServiceDepsMocked[key as keyof typeof wakServiceDepsMocked]
+      )
+  }
+
+  beforeAll(async (): Promise<void> => {
+    bindings = await createContainer(env)
+    appContainer = await createApp(bindings)
+    knex = appContainer.knex
+    authService = await bindings.resolve('authService')
+    walletAddressKeyService = await bindings.resolve('walletAddressKeyService')
+    prepareWSKDepsMock()
+  })
+
+  afterAll(async (): Promise<void> => {
+    appContainer.stop()
+    knex.destroy()
+  })
+
+  afterEach(async (): Promise<void> => {
+    await truncateTables(knex)
+  })
+
+  beforeEach(async () => {
+    await prepareRelatedData()
+  })
+
+  describe('Upload key', () => {
+    it('should insert new key', async () => {
+      const base64Key =
+        'ewogICJrdHkiOiAiT0tQIiwKICAiY3J2IjogIkVkMjU1MTkiLAogICJraWQiOiAidGVzdC1rZXktZWQyNTUxOSIsCiAgImQiOiAibjROaS1IcElTcFZPYm5RTVcwd09oQ0tST2FJS3FLdFdfMlpZYjJwOUtjVSIsCiAgIngiOiAiSnJRTGo1UF84OWlYRVM5LXZGZ3JJeTI5Y2xGOUNDX29QUHN3M2M1RDBicyIKfQ=='
+      await walletAddressKeyService.uploadKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname: faker.lorem.word(),
+        base64Key
+      })
+      const key = await WalletAddressKeys.query().first()
+      expect(key).toHaveProperty('id')
+      expect(key?.id).toBe('test-key-ed25519')
+      expect(key?.rafikiId).toBe(rafikiKeyId)
+      expect(key?.walletAddressId).toBe(walletAddress.id)
+      expect(key).toHaveProperty('nickname')
+      expect(key).toHaveProperty('publicKey')
+    })
+
+    it('should throw error if key is not valid', async () => {
+      const base64Key = ''
+      await expect(
+        walletAddressKeyService.uploadKey({
+          userId,
+          accountId: walletAddress.accountId,
+          walletAddressId: walletAddress.id,
+          nickname: faker.lorem.word(),
+          base64Key
+        })
+      ).rejects.toThrowError('Unexpected end of JSON input')
+    })
+  })
+
+  describe('Register key', () => {
+    it('should insert new key', async () => {
+      const nickname = faker.lorem.word()
+      const result = await walletAddressKeyService.registerKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname
+      })
+      const key = await WalletAddressKeys.query().first()
+      expect(key).toHaveProperty('id')
+      expect(key?.rafikiId).toBe(rafikiKeyId)
+      expect(key?.walletAddressId).toBe(walletAddress.id)
+      expect(key?.nickname).toBe(nickname)
+      expect(key).toHaveProperty('publicKey')
+
+      expect(result.keyId).toBe(key?.id)
+      expect(result).toHaveProperty('privateKey')
+      expect(result).toHaveProperty('publicKey')
+      expect(result.nickname).toBe(nickname)
+    })
+  })
+
+  describe('Revoke key', () => {
+    let newKey: WalletAddressKeys | undefined
+
+    beforeEach(async () => {
+      const nickname = faker.lorem.word()
+      await walletAddressKeyService.registerKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname
+      })
+
+      newKey = await WalletAddressKeys.query().first()
+    })
+
+    it('should delete key', async () => {
+      const spy = jest.spyOn(rafikiClientMock, 'revokeWalletAddressKey')
+
+      await walletAddressKeyService.revokeKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        keyId: newKey?.id ?? ''
+      })
+      const key = await WalletAddressKeys.query().first()
+      expect(key).toBeUndefined()
+      expect(spy).toHaveBeenCalledWith(newKey?.rafikiId)
+    })
+  })
+
+  describe('Patch key', () => {
+    let newKey: WalletAddressKeys | undefined
+
+    beforeEach(async () => {
+      const nickname = faker.lorem.word()
+      await walletAddressKeyService.registerKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname
+      })
+
+      newKey = await WalletAddressKeys.query().first()
+    })
+
+    it('should update key nickname', async () => {
+      await walletAddressKeyService.patch({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        keyId: newKey?.id ?? '',
+        nickname: 'test'
+      })
+
+      const key = await WalletAddressKeys.query().first()
+      expect(key?.nickname).toBe('test')
+    })
+  })
+
+  describe('List', () => {
+    let newKey: WalletAddressKeys | undefined
+
+    beforeEach(async () => {
+      const nickname = faker.lorem.word()
+      await walletAddressKeyService.registerKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname
+      })
+
+      newKey = await WalletAddressKeys.query().first()
+    })
+
+    it('should return list of keys', async () => {
+      const result = await walletAddressKeyService.listByWalletId({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id
+      })
+
+      expect(result).toMatchObject([newKey])
+    })
+
+    it('should return empty list', async () => {
+      const result = await walletAddressKeyService.listByWalletId({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: faker.string.uuid()
+      })
+
+      expect(result).toMatchObject([])
+    })
+  })
+
+  describe('Get by id', () => {
+    let newKey: WalletAddressKeys | undefined
+
+    beforeEach(async () => {
+      const nickname = faker.lorem.word()
+      await walletAddressKeyService.registerKey({
+        userId,
+        accountId: walletAddress.accountId,
+        walletAddressId: walletAddress.id,
+        nickname
+      })
+
+      newKey = await WalletAddressKeys.query().first()
+    })
+
+    it('should return key', async () => {
+      const result = await walletAddressKeyService.getById(
+        walletAddress.id,
+        newKey?.id ?? ''
+      )
+
+      expect(result).toMatchObject(newKey ?? {})
+    })
+
+    it('should return 404 for wrong wallet address id', async () => {
+      await expect(
+        walletAddressKeyService.getById(faker.string.uuid(), newKey?.id ?? '')
+      ).rejects.toThrowError(NotFound)
+    })
+
+    it('should return 404 for wrong key id', async () => {
+      await expect(
+        walletAddressKeyService.getById(walletAddress.id, faker.string.uuid())
+      ).rejects.toThrowError(NotFound)
+    })
+  })
+})
