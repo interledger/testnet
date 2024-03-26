193749eabd1698d6f046ae7e33232897be86e068
Rico <rico.gonzalez001@outlook.com>
2024-03-26 20:51:01 +0330

    Fix notfound TypedResponseBody

--- packages/shared/backend/src/utils.ts
+++ packages/shared/backend/src/utils.ts
@@ -1,4 +1,4 @@
-import { TypedResponseBody } from '@/types'
+import { TypedResponseBody } from './types'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isObject(value: unknown): value is Record<string, any> {
   return typeof value === 'object' && value !== null
