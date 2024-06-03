496932e870e93212af32b4bfe65ea9f10a7a9668
Rico <rico.gonzalez001@outlook.com>
2024-06-03 16:12:12 +0330

    fix format

--- packages/wallet/backend/src/grant/service.ts
+++ packages/wallet/backend/src/grant/service.ts
@@ -90,28 +90,31 @@ export class GrantService implements IGrantService {
       for (const accessElement of grant.access)
         if (accessElement.limits?.interval) {
           const time = accessElement.limits?.interval.split('/')
-          const isEnd = time[1].startsWith('P');
-          const duration = moment.duration(time.find(it=> it.startsWith('P')))
-          const date = moment(time.find(it=> it.endsWith('Z')))
-          const repetition = time.find(it=> it.startsWith('R'))?.[1]
+          const isEnd = time[1].startsWith('P')
+          const duration = moment.duration(
+            time.find((it) => it.startsWith('P'))
+          )
+          const date = moment(time.find((it) => it.endsWith('Z')))
+          const repetition = time.find((it) => it.startsWith('R'))?.[1]
 
-          if(repetition === '0')
-            accessElement.limits!.interval = `${isEnd? 'Until' : 'From'} ${date.format('MMMM Do YYYY')} with no repetition`;
-          else if(repetition === undefined)
-            accessElement.limits!.interval = `${this.processDuration(duration)} ${isEnd? 'until' : 'from'} ${date.format('MMMM Do YYYY')}`;
+          if (repetition === '0')
+            accessElement.limits!.interval = `${isEnd ? 'Until' : 'From'} ${date.format('MMMM Do YYYY')} with no repetition`
+          else if (repetition === undefined)
+            accessElement.limits!.interval = `${this.processDuration(duration)} ${isEnd ? 'until' : 'from'} ${date.format('MMMM Do YYYY')}`
           else
-            accessElement.limits!.interval = `${repetition} times ${this.processDuration(duration)} ${isEnd? 'until' : 'from'} ${date.format('MMMM Do YYYY')}`;
+            accessElement.limits!.interval = `${repetition} times ${this.processDuration(duration)} ${isEnd ? 'until' : 'from'} ${date.format('MMMM Do YYYY')}`
         }
   }
 
-  private processDuration(duration:moment.Duration){
-    const years = duration.years() !== 0 ? `${duration.years()} years, ` : '';
-    const months = duration.months()!== 0 ? `${duration.months()} months, ` : ''
-    const days = duration.days()!== 0 ? `${duration.days()} days, ` : ''
-    const hours = duration.hours()!== 0 ?`${duration.hours()} hours, `: ''
-    const minutes = duration.minutes()!== 0 ? `${duration.minutes()} minutes` : ''
+  private processDuration(duration: moment.Duration) {
+    const years = duration.years() !== 0 ? `${duration.years()} years, ` : ''
+    const months =
+      duration.months() !== 0 ? `${duration.months()} months, ` : ''
+    const days = duration.days() !== 0 ? `${duration.days()} days, ` : ''
+    const hours = duration.hours() !== 0 ? `${duration.hours()} hours, ` : ''
+    const minutes =
+      duration.minutes() !== 0 ? `${duration.minutes()} minutes` : ''
 
-    return `Every ${years}${months}${days}${hours}${minutes}`.replace(/, $/, '');
+    return `Every ${years}${months}${days}${hours}${minutes}`.replace(/, $/, '')
   }
-
 }
