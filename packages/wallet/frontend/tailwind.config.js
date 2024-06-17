c6e73856fb8eb314aaaa25de35600bbfc0d9fe36
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-06-17 13:40:40 +0300

    Add glows

--- packages/wallet/frontend/tailwind.config.js
+++ packages/wallet/frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 const twColors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
@@ -40,23 +41,17 @@ module.exports = {
       }
     },
     extend: {
-      backgroundImage: {
-        'gradient-primary': `linear-gradient(to right, #56AEAC, #8FD8C7)`,
-        'gradient-primary-dark': `linear-gradient(to right, #7acebe, #2b7576)`,
-        'gradient-secondary': `linear-gradient(to right, #E78CA3, #EFAB94)`,
-        'gradient-secondary-dark': `linear-gradient(to right, #d85b7d, #df7754)`,
-        'gradient-overlay': `linear-gradient(270deg, #92DBCA 0.16%, #56B1AF 100%)`,
-        'gradient-success': `linear-gradient(to right, #00A7CE 0%, #1AABC6 19.43%, #45B1B8 55.37%, #60B6B0 83.45%, #6AB7AD 100%)`,
-        'gradient-error': `linear-gradient(to right, #E489A0 1.13%, #E99E96 58.42%, #ECA891 100%)`,
-        'gradient-violet': `linear-gradient(to right, #9A8FCD, #9AB5D3)`,
-        'gradient-pink': `linear-gradient(to right, #FF9DA9, #FFBFD3)`,
-        'gradient-orange': `linear-gradient(to right, #FFB080, #E1CD96)`,
-        'gradient-backdrop': `linear-gradient(to right, #56B1AFE6, #92DBCAE6, #56B1AFE6)`
+      boxShadow: {
+        'glow-button': ['0 0 0.2rem #ffffff', '0 0 0.4rem #ffffff', '0 0 1rem #da35ba', '0 0 0.4rem #da35ba', '0 0 1.4rem #da35ba', 'inset 0 0 0.6rem #da35ba'],
+        'glow-link': '[0 0 0.2rem #ffffff, 0 0 0.2rem #ffffff, 0 0 1rem #da35ba, 0 0 0.4rem #da35ba, inset 0 0 0.6rem #da35ba]',
+      },
+      dropShadow: {
+        'glow-svg': ['0 0 0.2rem #fff', '0 0 1rem rgb(var(--pink-neon))', '0 0 0.4rem rgb(var(--pink-neon))'],
       },
       screens: {
         'h-sm': { raw: '(min-height: 600px)' }
       }
-    }
-  },
-  plugins: [require('@tailwindcss/forms'), require('tailwindcss-animate')]
+    },
+    plugins: [require('@tailwindcss/forms'), require('tailwindcss-animate')]
+  }
 }
