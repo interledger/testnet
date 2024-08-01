70fb990faed1acae45c073f98a4dd7eb850c93f8
bsanduc <bogdan.sandu@breakpointit.eu>
2024-08-01 20:27:05 +0300

    Prettier

--- packages/wallet/backend/migrations/20240801155211_update_wallet_addresses.js
+++ packages/wallet/backend/migrations/20240801155211_update_wallet_addresses.js
@@ -3,14 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-    await knex('walletAddresses').update({ assetScale: 9 });
-  };
-  
+  await knex('walletAddresses').update({ assetScale: 9 })
+}
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
-await knex('walletAddresses').update({ assetScale: 2 });
-};
-  
+  await knex('walletAddresses').update({ assetScale: 2 })
+}
