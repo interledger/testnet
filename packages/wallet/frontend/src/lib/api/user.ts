74349b80a5f84ce1599934235cada8c6d28957ec
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-09-04 10:42:59 +0300

    Update user service

--- packages/wallet/frontend/src/lib/api/user.ts
+++ packages/wallet/frontend/src/lib/api/user.ts
@@ -5,8 +5,6 @@ import {
   type ErrorResponse,
   type SuccessResponse
 } from '../httpClient'
-import { ACCEPTED_IMAGE_TYPES } from '@/utils/constants'
-import { SelectOption } from '@/ui/forms/Select'
 import { UserResponse, ValidTokenResponse } from '@wallet/shared'
 import { emailSchema } from '@wallet/shared/src'
 
@@ -76,66 +74,14 @@ export const loginSchema = z.object({
   password: z.string().min(1, { message: 'Password is required' })
 })
 
-export const personalDetailsSchema = z.object({
-  firstName: z.string().min(1, { message: 'First name is required' }),
-  lastName: z.string().min(1, { message: 'Last name is required' }),
-  country: z.object({
-    value: z.string().length(2),
-    label: z.string().min(1)
-  }),
-  city: z.string().min(1, { message: 'City is required' }),
-  address: z.string().min(1, { message: 'Address is required' }),
-  zip: z.string().min(1, { message: 'ZIP code is required' })
-})
-
 export const profileSchema = z.object({
   firstName: z.string().min(1, { message: 'First name is required' }),
   lastName: z.string().min(1, { message: 'Last name is required' })
 })
 
-export const verifyIdentitySchema = z
-  .object({
-    documentType: z.string({
-      invalid_type_error: 'Please select an ID Type'
-    }),
-    frontSideImage: z
-      .string()
-      .min(1, { message: 'Front side of ID is required' }),
-    frontSideImageType: z.string(),
-    backSideImage: z.string().optional(),
-    backSideImageType: z.string().optional(),
-    faceImage: z.string().min(1, { message: 'A selfie image is required' }),
-    faceImageType: z.string()
-  })
-  .superRefine(
-    ({ frontSideImageType, faceImageType, backSideImageType }, ctx) => {
-      if (!ACCEPTED_IMAGE_TYPES.includes(frontSideImageType)) {
-        ctx.addIssue({
-          code: 'custom',
-          message: `Image must be 'jpeg' or 'png'`,
-          path: ['frontSideImage']
-        })
-      }
-      if (!ACCEPTED_IMAGE_TYPES.includes(faceImageType)) {
-        ctx.addIssue({
-          code: 'custom',
-          message: `Image must be 'jpeg' or 'png'`,
-          path: ['faceImage']
-        })
-      }
-      if (
-        backSideImageType &&
-        backSideImageType?.length > 0 &&
-        !ACCEPTED_IMAGE_TYPES.includes(backSideImageType)
-      ) {
-        ctx.addIssue({
-          code: 'custom',
-          message: `Image must be 'jpeg' or 'png'`,
-          path: ['backSideImage']
-        })
-      }
-    }
-  )
+export const forgotPasswordSchema = z.object({
+  email: z.string().email({ message: 'Email is required' })
+})
 
 export const resetPasswordSchema = z
   .object({
@@ -197,11 +143,9 @@ export const changePasswordSchema = z
     }
   })
 
-export type Document = {
-  type: string
-  name: string
-  isBackRequired: boolean
-}
+const getBearerTokenSchema = z.object({
+  type: z.enum(['onboarding', 'ramp'])
+})
 
 type SignUpArgs = z.infer<typeof signUpSchema>
 type SignUpError = ErrorResponse<SignUpArgs | undefined>
@@ -239,14 +183,6 @@ type VerifyEmailResponse = SuccessResponse | VerifyEmailError
 type MeResult = SuccessResponse<UserResponse>
 type MeResponse = MeResult | ErrorResponse
 
-type CreateWalletArgs = z.infer<typeof personalDetailsSchema>
-type CreateWalletError = ErrorResponse<CreateWalletArgs | undefined>
-type CreateWalletResponse = SuccessResponse | CreateWalletError
-
-type VerifyIdentityArgs = z.infer<typeof verifyIdentitySchema>
-type VerifyIdentityError = ErrorResponse<VerifyIdentityArgs | undefined>
-type VerifyIdentityResponse = SuccessResponse | VerifyIdentityError
-
 type ProfileArgs = z.infer<typeof profileSchema>
 type ProfileError = ErrorResponse<ProfileArgs | undefined>
 type ProfileResponse = SuccessResponse | ProfileError
@@ -254,6 +190,11 @@ type ProfileResponse = SuccessResponse | ProfileError
 type ChangePasswordArgs = z.infer<typeof changePasswordSchema>
 type ChangePasswordError = ErrorResponse<ChangePasswordArgs | undefined>
 type ChangePasswordResponse = SuccessResponse | ChangePasswordError
+
+type GetBearerTokenArgs = z.infer<typeof getBearerTokenSchema>
+type GetBearerTokenResult = SuccessResponse<{ url: string }>
+type GetBearerTokenError = ErrorResponse<GetBearerTokenArgs | undefined>
+type GetBearerTokenResponse = GetBearerTokenResult | GetBearerTokenError
 
 interface UserService {
   signUp: (args: SignUpArgs) => Promise<SignUpResponse>
@@ -264,15 +205,12 @@ interface UserService {
   checkToken: (token: string, cookies?: string) => Promise<CheckTokenResponse>
   verifyEmail: (args: VerifyEmailArgs) => Promise<VerifyEmailResponse>
   me: (cookies?: string) => Promise<MeResponse>
-  createWallet: (args: CreateWalletArgs) => Promise<CreateWalletResponse>
-  verifyIdentity: (args: VerifyIdentityArgs) => Promise<VerifyIdentityResponse>
   updateProfile: (args: ProfileArgs) => Promise<ProfileResponse>
-  getDocuments: (cookies?: string) => Promise<Document[]>
-  getCountries: (cookies?: string) => Promise<SelectOption[]>
   changePassword: (args: ChangePasswordArgs) => Promise<ChangePasswordResponse>
   resendVerifyEmail: (
     args: ResendVerificationEmailArgs
   ) => Promise<ResendVerificationEmailResponse>
+  getBearerToken: (args: GetBearerTokenArgs) => Promise<GetBearerTokenResponse>
 }
 
 const createUserService = (): UserService => ({
@@ -415,41 +353,6 @@ const createUserService = (): UserService => ({
     }
   },
 
-  async createWallet(args) {
-    try {
-      const response = await httpClient
-        .post('wallet', {
-          json: {
-            ...args,
-            country: args.country.value
-          }
-        })
-        .json<SuccessResponse>()
-      return response
-    } catch (error) {
-      return getError<CreateWalletArgs>(
-        error,
-        'Something went wrong while trying to create your wallet. Please try again.'
-      )
-    }
-  },
-
-  async verifyIdentity(args) {
-    try {
-      const response = await httpClient
-        .post('verify', {
-          json: args
-        })
-        .json<SuccessResponse>()
-      return response
-    } catch (error) {
-      return getError<VerifyIdentityArgs>(
-        error,
-        'Something went wrong while verifying your ID. Please try again.'
-      )
-    }
-  },
-
   async updateProfile(args) {
     try {
       const response = await httpClient
@@ -466,36 +369,6 @@ const createUserService = (): UserService => ({
     }
   },
 
-  async getDocuments(cookies) {
-    try {
-      const response = await httpClient
-        .get('documents', {
-          headers: {
-            ...(cookies ? { Cookie: cookies } : {})
-          }
-        })
-        .json<SuccessResponse<Document[]>>()
-      return response?.result ?? []
-    } catch (error) {
-      return []
-    }
-  },
-
-  async getCountries(cookies) {
-    try {
-      const response = await httpClient
-        .get('countries', {
-          headers: {
-            ...(cookies ? { Cookie: cookies } : {})
-          }
-        })
-        .json<SuccessResponse<SelectOption[]>>()
-      return response?.result ?? []
-    } catch (error) {
-      return []
-    }
-  },
-
   async changePassword(args) {
     try {
       const response = await httpClient
@@ -508,6 +381,21 @@ const createUserService = (): UserService => ({
       return getError<ChangePasswordArgs>(
         error,
         'Something went wrong while changing your password. Please try again.'
+      )
+    }
+  },
+
+  async getBearerToken(args) {
+    try {
+      const response = await httpClient
+        .get(`gatehub/token/${args.type}`)
+        .json<GetBearerTokenResult>()
+      return response
+    } catch (error) {
+      return getError<GetBearerTokenArgs>(
+        error,
+        // TODO: Better error message
+        'Something went wrong. Please try again.'
       )
     }
   }
