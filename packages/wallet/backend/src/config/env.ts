a32ae8ab47616fc5491f4220424e8617660ca063
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-09-05 20:01:52 +0300

    remove Max asset scale env variable

--- packages/wallet/backend/src/config/env.ts
+++ packages/wallet/backend/src/config/env.ts
@@ -33,7 +33,6 @@ const envSchema = z.object({
     .default('false')
     .transform((value) => value === 'true'),
   BASE_ASSET_SCALE: z.coerce.number().nonnegative().default(2),
-  MAX_ASSET_SCALE: z.coerce.number().nonnegative().default(9),
   DEBT_THRESHOLD: z.coerce.number().multipleOf(0.01).nonnegative().default(5.0), // $5.00
   DEFAULT_WALLET_ACCOUNT: z
     .object({
