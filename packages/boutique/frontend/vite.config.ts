7c32d9bda37241215e5b939a2fab39e2a2eb6721
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-09-20 23:33:10 +0300

    discard unused code

--- packages/boutique/frontend/vite.config.ts
+++ packages/boutique/frontend/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export default defineConfig({
-  define: {
-    VITE_CURRENCY: `'${process.env.VITE_CURRENCY}'`
-  },
   plugins: [react()],
   server: {
     port: 4004
