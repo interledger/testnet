f94895d9f51b3d22f565f1621ba4761736b9cd69
Adrian Boros <adiboros@hotmail.com>
2024-09-23 12:40:13 +0300

    fix lint issues

--- packages/wallet/frontend/src/components/icons/Eye.tsx
+++ packages/wallet/frontend/src/components/icons/Eye.tsx
@@ -16,23 +16,23 @@ export const Eye = ({ loading, ...props }: EyeProps) => {
     >
       <path
         d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <circle
         cx="12"
         cy="12"
         r="3"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
--- packages/wallet/frontend/src/components/icons/SlashEye.tsx
+++ packages/wallet/frontend/src/components/icons/SlashEye.tsx
@@ -16,21 +16,21 @@ export const SlashEye = ({ loading, ...props }: SlashEyeProps) => {
     >
       <path
         d="M2 2L22 22"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
