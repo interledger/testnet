ed4fa2f2a7f1dc30f833b0169c7b29ddd1c8d607
dragosp1011 <dragosh1011@gmail.com>
2024-09-25 14:12:08 +0200

    fix: request the correct iframe type

--- packages/wallet/frontend/src/pages/withdraw.tsx
+++ packages/wallet/frontend/src/pages/withdraw.tsx
@@ -39,7 +39,7 @@ export const getServerSideProps: GetServerSideProps<{
   url: string
 }> = async (ctx) => {
   const response = await userService.getGateHubIframeSrc(
-    { type: 'ramp' },
+    'withdrawal',
     ctx.req.headers.cookie
   )
 
