68698e31c2347d67f232e191688f6320175de610
dragosp1011 <109967337+dragosp1011@users.noreply.github.com>
2024-09-26 11:51:22 +0300

    feat(wallet): replace rapyd withdrawal with gatehub (#1651)
    * Update user service
    
    * Add standalone KYC page
    
    * Add TODO comment for settings page
    
    * Format; Update KYC Page; fix `getBearerToken` method
    
    * Remove old KYC pages
    
    * Format with latest prettier
    
    * Remove Rapyd env/constants
    
    * Remove Rapyd mocks
    
    * Better naming
    
    * Format
    
    * Add standalone deposit page; remove old fund dialog
    
    * Add standalone withdraw page
    
    * Remove Rapyd withdraw features
    
    * Format
    
    * Update withdraw.tsx
    
    * Update deposit.tsx
    
    * Revert "Update deposit.tsx"
    
    This reverts commit 5da2dba306a5b0df4e5412c6c35f47a675f7a89b.
    
    * Revert "Update withdraw.tsx"
    
    This reverts commit 19ceeec195cdbc75d03f390b45e52abcac218109.
    
    * fix: request the correct iframe type
    
    * Update withdraw.tsx
    
    ---------
    
    Co-authored-by: Radu-Cristian Popa <radu.popa@breakpointit.eu>
    Co-authored-by: Tymmmy <117268143+Tymmmy@users.noreply.github.com>

--- packages/wallet/frontend/src/lib/api/account.ts
+++ packages/wallet/frontend/src/lib/api/account.ts
@@ -5,7 +5,7 @@ import {
   type ErrorResponse,
   type SuccessResponse
 } from '../httpClient'
-import { createAccountSchema, withdrawFundsSchema } from '@wallet/shared'
+import { createAccountSchema } from '@wallet/shared'
 import { WalletAddressResponse } from '@wallet/shared/src'
 
 export type Account = {
@@ -29,10 +29,6 @@ type CreateAccountResult = SuccessResponse<Account>
 type CreateAccountError = ErrorResponse<CreateAccountArgs | undefined>
 type CreateAccountResponse = CreateAccountResult | CreateAccountError
 
-type WithdrawFundsArgs = z.infer<typeof withdrawFundsSchema>
-type WithdrawFundsError = ErrorResponse<WithdrawFundsArgs | undefined>
-type WithdrawFundsResponse = SuccessResponse | WithdrawFundsError
-
 interface AccountService {
   get: (accountId: string, cookies?: string) => Promise<GetAccountResponse>
   list: (
@@ -40,7 +36,6 @@ interface AccountService {
     include?: ('walletAddresses' | 'walletAddressKeys')[]
   ) => Promise<ListAccountsResponse>
   create: (args: CreateAccountArgs) => Promise<CreateAccountResponse>
-  withdraw: (args: WithdrawFundsArgs) => Promise<WithdrawFundsResponse>
 }
 
 const createAccountService = (): AccountService => ({
@@ -95,22 +90,6 @@ const createAccountService = (): AccountService => ({
       return getError<CreateAccountArgs>(
         error,
         'We were not able to create your account. Please try again.'
-      )
-    }
-  },
-
-  async withdraw(args) {
-    try {
-      const response = await httpClient
-        .post('accounts/withdraw', {
-          json: args
-        })
-        .json<SuccessResponse>()
-      return response
-    } catch (error) {
-      return getError<WithdrawFundsArgs>(
-        error,
-        'We were not able to withdraw the funds. Please try again.'
       )
     }
   }
