e94bed1799d0ba96eeaff9160f47dd6a3788e757
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-10-11 00:15:05 +0300

    Replace `staging` with `sandbox`

--- packages/wallet/frontend/next.config.js
+++ packages/wallet/frontend/next.config.js
@@ -6,7 +6,7 @@ let NEXT_PUBLIC_FEATURES_ENABLED = 'true'
 
 if (
   process.env.NODE_ENV === 'production' &&
-  process.env.GATEHUB_ENV === 'staging'
+  process.env.GATEHUB_ENV === 'sandbox'
 ) {
   NEXT_PUBLIC_FEATURES_ENABLED = 'false'
 }
