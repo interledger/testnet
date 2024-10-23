795347b948282ef7212185c629d73ca0129081a7
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-10-23 14:44:57 +0200

    Update [accountId].tsx

--- packages/wallet/frontend/src/pages/account/[accountId].tsx
+++ packages/wallet/frontend/src/pages/account/[accountId].tsx
@@ -65,52 +65,54 @@ const AccountPage: NextPageWithLayout<AccountPageProps> = ({
         <h2 className="mb-2 text-xl">Balance</h2>
         <div className="text-3xl font-bold">{formattedAmount.amount}</div>
       </div>
-      <div className="my-12 flex gap-3 md:max-w-lg">
-        <button
-          id="walletAddress"
-          onClick={() => {
-            if (isUserFirstTime) {
-              setRunOnboarding(false)
+      <div className="my-12 flex md:max-w-lg gap-4 flex-col sm:flex-row">
+        <div className="flex gap-4">
+          <button
+            id="walletAddress"
+            onClick={() => {
+              if (isUserFirstTime) {
+                setRunOnboarding(false)
+              }
+              openDialog(
+                <CreateWalletAddressDialog
+                  accountName={account.name}
+                  onClose={closeDialog}
+                />
+              )
+            }}
+            className="group flex aspect-square min-w-28 flex-shrink-0 flex-grow-0 basis-1/4 flex-col items-center justify-center rounded-lg border-2 text-center transition-[box-shadow] duration-200 dark:hover:shadow-glow-button dark:focus:shadow-glow-button"
+          >
+            <New className="mb-1 h-8 w-8 transition-[filter] duration-200 group-hover:dark:drop-shadow-glow-svg group-focus:dark:drop-shadow-glow-svg" />
+            <span className="text-center text-[smaller] leading-4 underline-offset-2 transition-transform group-hover:scale-110 group-hover:underline group-focus:scale-110 group-focus:underline group-focus:underline-offset-2 dark:group-hover:decoration-transparent">
+              Add payment pointer
+            </span>
+          </button>
+          <Link
+            id="fund"
+            href={FEATURES_ENABLED ? '/deposit' : undefined}
+            onClick={
+              FEATURES_ENABLED
+                ? undefined
+                : () => {
+                    openDialog(
+                      <DepositDialog
+                        accountId={account.id}
+                        assetCode={account.assetCode}
+                        onClose={closeDialog}
+                      />
+                    )
+                  }
             }
-            openDialog(
-              <CreateWalletAddressDialog
-                accountName={account.name}
-                onClose={closeDialog}
-              />
-            )
-          }}
-          className="group flex aspect-square min-w-28 flex-shrink-0 flex-grow-0 basis-1/4 flex-col items-center justify-center rounded-lg border-2 text-center transition-[box-shadow] duration-200 dark:hover:shadow-glow-button dark:focus:shadow-glow-button"
-        >
-          <New className="mb-1 h-8 w-8 transition-[filter] duration-200 group-hover:dark:drop-shadow-glow-svg group-focus:dark:drop-shadow-glow-svg" />
-          <span className="text-center text-[smaller] leading-4 underline-offset-2 transition-transform group-hover:scale-110 group-hover:underline group-focus:scale-110 group-focus:underline group-focus:underline-offset-2 dark:group-hover:decoration-transparent">
-            Add payment pointer
-          </span>
-        </button>
-        <Link
-          id="fund"
-          href={FEATURES_ENABLED ? '/deposit' : undefined}
-          onClick={
-            FEATURES_ENABLED
-              ? undefined
-              : () => {
-                  openDialog(
-                    <DepositDialog
-                      accountId={account.id}
-                      assetCode={account.assetCode}
-                      onClose={closeDialog}
-                    />
-                  )
-                }
-          }
-          className="group flex aspect-square min-w-28 flex-shrink-0 flex-grow-0 basis-1/4 flex-col items-center justify-center rounded-lg border-2 text-center transition-[box-shadow] duration-200 dark:hover:shadow-glow-button dark:focus:shadow-glow-button"
-        >
-          <Request className="mb-1 h-8 w-8 transition-[filter] duration-200 group-hover:dark:drop-shadow-glow-svg group-focus:dark:drop-shadow-glow-svg" />
-          <span className="text-center text-[smaller] leading-4 underline-offset-2 transition-transform group-hover:scale-110 group-hover:underline group-focus:scale-110 group-focus:underline group-focus:underline-offset-2 dark:group-hover:decoration-transparent">
-            Deposit
-          </span>
-        </Link>
+            className="group flex aspect-square min-w-28 flex-shrink-0 flex-grow-0 basis-1/4 flex-col items-center justify-center rounded-lg border-2 text-center transition-[box-shadow] duration-200 dark:hover:shadow-glow-button dark:focus:shadow-glow-button"
+          >
+            <Request className="mb-1 h-8 w-8 transition-[filter] duration-200 group-hover:dark:drop-shadow-glow-svg group-focus:dark:drop-shadow-glow-svg" />
+            <span className="text-center text-[smaller] leading-4 underline-offset-2 transition-transform group-hover:scale-110 group-hover:underline group-focus:scale-110 group-focus:underline group-focus:underline-offset-2 dark:group-hover:decoration-transparent">
+              Deposit
+            </span>
+          </Link>
+        </div>
         {FEATURES_ENABLED ? (
-          <>
+          <div className="flex gap-4">
             <Link
               id="withdraw"
               href="/withdraw"
@@ -131,7 +133,7 @@ const AccountPage: NextPageWithLayout<AccountPageProps> = ({
                 Card
               </span>
             </Link>
-          </>
+          </div>
         ) : null}
       </div>
       <h2 className="mb-2 text-2xl font-bold">Payment Pointers</h2>
