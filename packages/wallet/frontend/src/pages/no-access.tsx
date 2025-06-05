d57d2d7b0acb2be925211eeeeeeeb5c7cf961972
dragosp1011 <dragosh1011@gmail.com>
2025-06-05 17:55:17 +0300

    fix: finish interaction on no access page

--- packages/wallet/frontend/src/pages/no-access.tsx
+++ packages/wallet/frontend/src/pages/no-access.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { THEME } from '@/utils/constants'
+import { useRouter } from 'next/router'
 
 const NoAccessPage = () => {
+  const router = useRouter()
+  const { interactionId, nonce } = router.query
   const imageName =
     THEME === 'dark' ? '/bird-error-dark.webp' : '/bird-error-light.webp'
+
+  const link = `${process.env.NEXT_PUBLIC_AUTH_HOST}/interact/${interactionId}/${nonce}/finish`
 
   return (
     <div className="bg-green-light dark:bg-purple flex h-full w-screen flex-col items-center justify-center text-center text-2xl font-semibold">
       <h1>Access to the given resource is forbidden.</h1>
       <Link
-        href="/"
+        href={link}
         className="dark:hover:text-pink-neon mt-5 text-xl font-semibold underline hover:text-green"
       >
-        Go to home page
+        Go back
       </Link>
       <Image
         className="mt-10 object-cover"
