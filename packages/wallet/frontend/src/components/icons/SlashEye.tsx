017f0051c54637b3aa23586bdab39f95c5e653f1
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2026-03-03 10:40:23 +0200

    linting errors fix

--- packages/wallet/frontend/src/components/icons/SlashEye.tsx
+++ packages/wallet/frontend/src/components/icons/SlashEye.tsx
@@ -36,6 +36,7 @@ export const SlashEye = (props: SVGProps<SVGSVGElement>) => {
       viewBox="0 0 534 667"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
         fillRule="evenodd"
