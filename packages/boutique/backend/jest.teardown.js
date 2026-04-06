5b8f7645ed203c7a3113465c3e9ec403e3a0961f
Stephan Butler <stephan@interledger.foundation>
2026-04-06 16:24:13 +0200

    test: wallet unittest configuration fixes

--- packages/boutique/backend/jest.teardown.js
+++ packages/boutique/backend/jest.teardown.js
@@ -1,5 +1,8 @@
 module.exports = async () => {
-  if (global.__TESTING_POSTGRES_CONTAINER__) {
-    await global.__TESTING_POSTGRES_CONTAINER__.stop()
+  if (global.__POSTGRES_CONTAINER__) {
+    await global.__POSTGRES_CONTAINER__.stop()
+  }
+  if (global.__TESTING_REDIS_CONTAINER__) {
+    await global.__TESTING_REDIS_CONTAINER__.stop()
   }
 }
