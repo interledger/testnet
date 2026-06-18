b1a54ca4acb33f2aceaeed4b13817a5a4000346a
Stephan Butler <stephan.butler@deepsouthdevels.net>
2026-06-18 13:06:17 +0200

    feat(boutique): boutique configuration at deploy time (#2210)

--- packages/boutique/backend/jest.setup.js
+++ packages/boutique/backend/jest.setup.js
@@ -4,7 +4,6 @@ const { randomBytes, generateKeyPairSync } = require('crypto')
 const POSTGRES_PASSWORD = 'password'
 const POSTGRES_DB = randomBytes(16).toString('hex')
 const POSTGRES_PORT = 5432
-const REDIS_PORT = 6379
 
 module.exports = async () => {
   const container = await new GenericContainer('postgres:15')
@@ -13,10 +12,6 @@ module.exports = async () => {
       POSTGRES_DB
     })
     .withExposedPorts(POSTGRES_PORT)
-    .start()
-
-  const redisContainer = await new GenericContainer('redis:7')
-    .withExposedPorts(REDIS_PORT)
     .start()
 
   // env.ts requires these variables without defaults, so we supply
@@ -37,8 +32,6 @@ module.exports = async () => {
       })
       .trim()
   ).toString('base64')
-  process.env.REDIS_URL = `redis://localhost:${redisContainer.getMappedPort(REDIS_PORT)}/0`
 
   global.__POSTGRES_CONTAINER__ = container
-  global.__TESTING_REDIS_CONTAINER__ = redisContainer
 }
