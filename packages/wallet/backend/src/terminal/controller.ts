453f6b10ebf10a4fff797bdb79b648f907a875d6
MihaiAndrei98 <69770699+MihaiAndrei98@users.noreply.github.com>
2026-08-30 23:12:27 +0300

    feat: Verify merchant data and link to POS device

--- packages/wallet/backend/src/terminal/controller.ts
+++ packages/wallet/backend/src/terminal/controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 import { TerminalService } from './service'
 import { toSuccessResponse } from '@shared/backend'
+import { requestSchema } from './validation'
+import { WalletAddressService } from '@/walletAddress/service'
+import { validate } from '@/shared/validate'
 
 export class TerminalController {
-  constructor(private terminalService: TerminalService) {}
+  constructor(
+    private terminalService: TerminalService,
+    private walletAddressService: WalletAddressService
+  ) {}
 
   getOnboardingFormDefinition = async (
     _req: Request,
@@ -14,6 +20,39 @@ export class TerminalController {
       const formDefinition =
         await this.terminalService.getOnboardingFormDefinition()
       res.status(200).json(toSuccessResponse(formDefinition))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  validation = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const formDefinition =
+        await this.terminalService.getOnboardingFormDefinition()
+      const schema = await requestSchema(formDefinition)
+      const { body } = await validate(schema, req)
+
+      // check if walletAddress exists
+      const paymentPointerField = formDefinition.find(
+        (value) => value.validation?.format === 'payment-pointer'
+      )
+
+      if (paymentPointerField) {
+        const checkWalletAddress = await this.walletAddressService.getByUrl(
+          req.body[paymentPointerField.key]
+        )
+        if (!checkWalletAddress) {
+          return res.status(400).json({
+            valid: false,
+            error: {
+              field: paymentPointerField.key,
+              message: 'Wallet Address not found!'
+            }
+          })
+        }
+      }
+
+      res.status(200).json({ valid: true, response: body })
     } catch (error) {
       next(error)
     }
